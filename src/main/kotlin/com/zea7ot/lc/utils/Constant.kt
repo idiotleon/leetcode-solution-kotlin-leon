c73d56a8930f3d90457c0ddaa995f8f0367e278b
Leon <yanglyu.leon.7@gmail.com>
2020-09-13 00:48:39 -0700

    reviews

--- src/main/kotlin/com/zea7ot/lc/utils/Constant.kt
+++ src/main/kotlin/com/zea7ot/lc/utils/Constant.kt
@@ -4,6 +4,7 @@ class Constant {
     class Annotation {
         companion object {
             const val UNUSED = "unused"
+            const val UNUSED_PARAMETER = "UNUSED_PARAMETER"
         }
     }
 }
