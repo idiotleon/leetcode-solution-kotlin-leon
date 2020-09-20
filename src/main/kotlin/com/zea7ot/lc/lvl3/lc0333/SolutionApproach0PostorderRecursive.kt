7b9a20cce8bb5fa4cdaaeeff211854e7ef5fc7e4
Leon <yanglyu.leon.7@gmail.com>
2020-09-20 00:28:19 -0700

    review

---
+++ src/main/kotlin/com/zea7ot/lc/lvl3/lc0333/SolutionApproach0PostorderRecursive.kt
@@ -0,0 +1,41 @@
+/**
+ * https://leetcode.com/problems/largest-bst-subtree/
+ *
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(H)
+ *
+ * References:
+ *  https://leetcode.com/problems/largest-bst-subtree/discuss/78899/Very-Short-Simple-Java-O(N)-Solution
+ */
+package com.zea7ot.lc.lvl3.lc0333
+
+import com.zea7ot.lc.utils.Constant.Annotation.Companion.UNUSED
+import com.zea7ot.lc.utils.data_structure.tree.TreeNode
+
+@Suppress(UNUSED)
+class SolutionApproach0PostorderRecursive {
+    fun largestBSTSubtree(root: TreeNode?): Int {
+        val res = postorder(root)
+        return res.size
+    }
+
+    private fun postorder(node: TreeNode?): Result{
+        if(node == null)
+            return Result(Int.MAX_VALUE, Int.MIN_VALUE, 0)
+
+        val left = postorder(node.left)
+        val right = postorder(node.right)
+
+        return if(node.`val` > left.max && node.`val` < right.min){
+            val min = minOf(node.`val`, left.min)
+            val max = maxOf(node.`val`, right.max)
+            val size = left.size + right.size + 1
+            Result(min, max, size)
+        }else{
+            val size = maxOf(left.size, right.size)
+            Result(Integer.MIN_VALUE, Integer.MAX_VALUE, size)
+        }
+    }
+
+    private data class Result(val min: Int, val max: Int, val size: Int)
+}
