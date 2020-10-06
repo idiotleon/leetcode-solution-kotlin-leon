005155d67cf5443450457b42dbe69fd377eefea3
Leon <yanglyu.leon.7@gmail.com>
2020-10-06 16:24:03 -0700

    review

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl3/lc0503/SolutionApproach0MonoStack.kt
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/next-greater-element-ii/
+ *
+ * Time Complexity:     O(`totalNums`)
+ * Space Complexity:    O(`totalNums`)
+ *
+ * to maintain a "non-increasing" stack
+ *  1. the contents are indexes, instead of values
+ *  2. the values/elements, accessed by those indexes, are non-increasing
+ */
+package com.zea7ot.leetcode.lvl3.lc0503
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+import java.util.*
+
+@Suppress(UNUSED)
+class SolutionApproach0MonoStack {
+    fun nextGreaterElements(nums: IntArray): IntArray {
+
+        val totalNums = nums.size
+        val ans = IntArray(totalNums) { -1 }
+
+        val stack = LinkedList<Int>()
+
+        var idx = 0
+
+        while (idx < totalNums * 2) {
+            while (stack.isNotEmpty() && nums[idx % totalNums] > nums[stack.peek()]) {
+                ans[stack.pop()] = nums[idx % totalNums]
+            }
+
+            stack.push(idx % totalNums)
+            ++idx
+        }
+
+        return ans
+    }
+}
