7daf60c684dbcc667b7807c51da1390f22c5b838
Leon <yanglyu.leon.7@gmail.com>
2020-10-07 10:13:17 -0700

    review

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl3/lc0061/SolutionApproach0LinearScan.kt
@@ -0,0 +1,45 @@
+/**
+ * https://leetcode.com/problems/rotate-list/
+ *
+ * Time Complexity:     O(L)
+ * Space Complexity:    O(1)
+ *
+ * to link the tail of the list with the head, making it a circle
+ *
+ * References:
+ *  https://leetcode.com/problems/rotate-list/discuss/22751/Clean-Java-Solution-with-Brief-Explanation
+ */
+package com.zea7ot.leetcode.lvl3.lc0061
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+import com.zea7ot.leetcode.utils.dataStructure.ListNode
+
+@Suppress(UNUSED)
+class SolutionApproach0LinearScan {
+    fun rotateRight(head: ListNode?, k: Int): ListNode? {
+        // sanity check
+        if (head == null) return head
+
+        var cur = head
+        var len = 1
+        while (cur?.next != null) {
+            cur = cur.next
+            ++len
+        }
+
+        // to make the linked list circular
+        cur?.next = head
+
+        var copy = head
+        for (i in len - k % len downTo 2) {
+            copy = copy?.next
+        }
+
+        cur = copy?.next
+
+        // to break the linked list
+        copy?.next = null
+
+        return cur
+    }
+}
