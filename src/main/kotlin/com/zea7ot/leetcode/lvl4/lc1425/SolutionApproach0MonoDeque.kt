8cabe2e072555f638bed72f05fc4b5b6798d8244
Leon <yanglyu.leon.7@gmail.com>
2020-10-08 17:29:47 -0700

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc1425/SolutionApproach0MonoDeque.kt
@@ -0,0 +1,48 @@
+/**
+ * https://leetcode.com/problems/constrained-subsequence-sum/
+ *
+ * Time Complexity:     O(`totalNums`)
+ * Space Complexity:    O(`totalNums`)
+ *
+ * References:
+ *  https://leetcode.com/problems/constrained-subsequence-sum/discuss/597751/JavaC++Python-O(N)-Decreasing-Deque/675105
+ *  https://leetcode.com/problems/constrained-subsequence-sum/discuss/597751/JavaC++Python-O(N)-Decreasing-Deque
+ */
+package com.zea7ot.leetcode.lvl4.lc1425
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+import java.util.*
+
+@Suppress(UNUSED)
+class SolutionApproach0MonoDeque {
+    fun constrainedSubsetSum(nums: IntArray, k: Int): Int {
+
+        val totalNums = nums.size
+        val sums = IntArray(totalNums)
+        var sum = nums[0]
+
+        val deque = LinkedList<Int>()
+
+        for (idx in 0 until totalNums) {
+            sums[idx] = nums[idx]
+            if (deque.isNotEmpty()) {
+                sums[idx] += sums[deque.peek()]
+            }
+            sum = maxOf(sum, sums[idx])
+
+            if (deque.isNotEmpty() && deque.peekFirst() < (idx - k + 1)) {
+                deque.pollFirst()
+            }
+
+            while (deque.isNotEmpty() && sums[deque.peekLast()] <= sums[idx]) {
+                deque.pollLast()
+            }
+
+            if (sums[idx] > 0) {
+                deque.offer(idx)
+            }
+        }
+
+        return sum
+    }
+}
---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc1438/SolutionApproach0MonoDeque.kt
@@ -0,0 +1,43 @@
+package com.zea7ot.leetcode.lvl4.lc1438
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+import java.util.*
+
+@Suppress(UNUSED)
+class SolutionApproach0MonoDeque {
+    fun longestSubarray(nums: IntArray, limit: Int): Int {
+        val totalNums = nums.size
+
+        val maxDeque = LinkedList<Int>()
+        val minDeque = LinkedList<Int>()
+
+        var lo = 0
+        var hi = 0
+
+        while (hi < totalNums) {
+            while (maxDeque.isNotEmpty() && nums[hi] > maxDeque.peekLast()) {
+                maxDeque.pollLast()
+            }
+
+            while (minDeque.isNotEmpty() && nums[hi] < minDeque.peekLast()) {
+                minDeque.pollLast()
+            }
+
+            maxDeque.add(nums[hi])
+            minDeque.add(nums[hi])
+
+            if (maxDeque.peek() - minDeque.peek() > limit) {
+                if (maxDeque.peek() == nums[lo]) maxDeque.poll()
+
+                if (minDeque.peek() == nums[lo]) minDeque.poll()
+
+                ++lo
+            }
+
+
+            ++hi
+        }
+
+        return hi - lo
+    }
+}
