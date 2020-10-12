77c688900c5cda582ae1b96acb93854c2f76cd73
Leon <yanglyu.leon.7@gmail.com>
2020-10-12 00:48:32 -0700

    reviews

--- src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0072/SolutionApproach0DP2Dimen1.kt
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0072/SolutionApproach0DP2Dimen1.kt
@@ -14,31 +14,30 @@ package com.zea7ot.leetcode.lvl4.lc0072
 import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
 
 @Suppress(UNUSED)
-class SolutionApproach0DP2Dimen {
+class SolutionApproach0DP2Dimen1 {
     fun minDistance(word1: String, word2: String): Int {
         val len1 = word1.length
         val len2 = word2.length
 
-        val costs = Array(len1 + 1) { idx1 ->
-            IntArray(len2 + 1) { idx2 ->
-                when {
-                    idx1 == 0 -> idx2
-                    idx2 == 0 -> idx1
-                    else -> 0
-                }
-            }
+        val costs = Array(len1 + 1) { IntArray(len2 + 1) { 0 } }
+        for (idx1 in 0..len1) {
+            costs[idx1][0] = idx1
+        }
+        for (idx2 in 0..len2) {
+            costs[0][idx2] = idx2
         }
 
         for (idx1 in 0 until len1) {
             for (idx2 in 0 until len2) {
-                costs[idx1 + 1][idx2 + 1] = if (word1[idx1] == word2[idx2]) {
-                    costs[idx1][idx2]
+                if (word1[idx1] == word2[idx2]) {
+                    costs[idx1 + 1][idx2 + 1] = costs[idx1][idx2]
                 } else {
                     val insert = costs[idx1][idx2 + 1]
                     val replace = costs[idx1][idx2]
                     val delete = costs[idx1 + 1][idx2]
 
-                    1 + minOf(insert, replace, delete)
+                    costs[idx1 + 1][idx2 + 1] = minOf(insert, replace, delete)
+                    ++costs[idx1 + 1][idx2 + 1]
                 }
             }
         }
