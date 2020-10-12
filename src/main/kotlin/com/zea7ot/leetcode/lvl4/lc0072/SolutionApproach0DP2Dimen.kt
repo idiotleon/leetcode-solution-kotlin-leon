c068ad63cd4b13ddc6b751ea8be1159be8e1a326
Leon <yanglyu.leon.7@gmail.com>
2020-10-12 00:23:17 -0700

    review

--- src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0072/SolutionApproach0DP2Dimen.kt
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0072/SolutionApproach0DP2Dimen.kt
@@ -19,30 +19,29 @@ class SolutionApproach0DP2Dimen {
         val len1 = word1.length
         val len2 = word2.length
 
-        val dp = Array(len1 + 1) { IntArray(len2 + 1) { 0 } }
-        for (row in 0 until len1 + 1) {
-            dp[row][0] = row
+        val costs = Array(len1 + 1) { IntArray(len2 + 1) { 0 } }
+        for (idx1 in 0..len1) {
+            costs[idx1][0] = idx1
         }
-
-        for (col in 0 until len2 + 1) {
-            dp[0][col] = col
+        for (idx2 in 0..len2) {
+            costs[0][idx2] = idx2
         }
 
         for (idx1 in 0 until len1) {
             for (idx2 in 0 until len2) {
                 if (word1[idx1] == word2[idx2]) {
-                    dp[idx1 + 1][idx2 + 1] = dp[idx1][idx2]
+                    costs[idx1 + 1][idx2 + 1] = costs[idx1][idx2]
                 } else {
-                    val replace = dp[idx1][idx2]
-                    val insert = dp[idx1][idx2 + 1]
-                    val delete = dp[idx1 + 1][idx2]
+                    val insert = costs[idx1][idx2 + 1]
+                    val replace = costs[idx1][idx2]
+                    val delete = costs[idx1 + 1][idx2]
 
-                    val minCost = minOf(replace, insert, delete)
-                    dp[idx1 + 1][idx2 + 1] = minCost + 1
+                    costs[idx1 + 1][idx2 + 1] = minOf(insert, replace, delete)
+                    ++costs[idx1 + 1][idx2 + 1]
                 }
             }
         }
 
-        return dp[len1][len2]
+        return costs[len1][len2]
     }
 }
