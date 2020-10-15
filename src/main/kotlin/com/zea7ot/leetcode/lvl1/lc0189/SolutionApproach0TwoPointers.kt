4cd4aa6bdd0c883bfff6f84ffa5418507da6b3ba
Leon <yanglyu.leon.7@gmail.com>
2020-10-15 09:10:34 -0700

    review

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl1/lc0189/SolutionApproach0TwoPointers.kt
@@ -0,0 +1,36 @@
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/rotate-array/
+ *
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(1)
+ */
+package com.zea7ot.leetcode.lvl1.lc0189
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0TwoPointers {
+    fun rotate(nums: IntArray, k: Int) {
+        val totalNums = nums.size
+        val steps = k % totalNums
+
+        reverse(0, totalNums, nums)
+        reverse(0, steps, nums)
+        reverse(steps, totalNums, nums)
+    }
+
+    private fun reverse(low: Int, high: Int, nums: IntArray) {
+        var lo = low
+        var hi = high - 1
+
+        while (lo < hi) {
+            val temp = nums[lo]
+            nums[lo] = nums[hi]
+            nums[hi] = temp
+
+            ++lo
+            --hi
+        }
+    }
+}
