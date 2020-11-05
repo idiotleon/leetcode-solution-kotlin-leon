2c310a12e1e2637e2485da5d47715ed6a17ce302
Leon <yanglyu.leon.7@gmail.com>
2020-11-04 18:15:34 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/ood/lvl3/lc1244/SolutionApproach0TreeSet.kt
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/design-a-leaderboard/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/design-a-leaderboard/discuss/428412/Java-Map-and-TreeSet-(O(logN))
+ */
+package com.zea7ot.leetcode.ood.lvl3.lc1244
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0TreeSet {
+}
---
+++ src/main/kotlin/com/zea7ot/leetcode/ood/lvl3/lc1244/followups/followup0/Solution.kt
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/design-a-leaderboard/
+ *
+ * to add support for getRankByID()
+ *  and considering high concurrency and large volume of data(10 million record)
+ *
+ * References:
+ *  https://leetcode.com/problems/design-a-leaderboard/discuss/418833/Java-TreeMap-+-Map-Solution/731504
+ */
+package com.zea7ot.leetcode.ood.lvl3.lc1244.followups.followup0
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+
+@Suppress(UNUSED)
+class Solution {
+}
