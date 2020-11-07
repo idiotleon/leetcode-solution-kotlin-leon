61e1baeff55d16e69a6f7f461d60b200441e65e0
Leon <yanglyu.leon.7@gmail.com>
2020-11-07 10:29:14 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl3/lc0445/SolutionApproach0Stack.kt
@@ -0,0 +1,41 @@
+package com.zea7ot.leetcode.lvl3.lc0445
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+import com.zea7ot.leetcode.utils.dataStructure.linkedList.ListNode
+import java.util.*
+
+@Suppress(UNUSED)
+class SolutionApproach0Stack {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        val stack1 = LinkedList<Int>()
+        var cur = l1
+        while (cur != null) {
+            stack1.push(cur.`val`)
+            cur = cur.next
+        }
+
+        val stack2 = LinkedList<Int>()
+        cur = l2
+        while (cur != null) {
+            stack2.push(cur.`val`)
+            cur = cur.next
+        }
+
+        var carry = 0
+        var head: ListNode? = null
+
+        while (stack1.isNotEmpty() || stack2.isNotEmpty() || carry > 0) {
+            val num1 = if (stack1.isNotEmpty()) stack1.pop() else 0
+            val num2 = if (stack2.isNotEmpty()) stack2.pop() else 0
+
+            val sum = num1 + num2 + carry
+            carry = sum / 10
+
+            val newNode = ListNode(sum % 10)
+            newNode.next = head
+            head = newNode
+        }
+
+        return head
+    }
+}
---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0954/SolutionApproach0TreeMap1.kt
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/array-of-doubled-pairs/
+ *
+ * Time Complexity:     O(`nNums` * distinct(`nNums`))
+ * Space Complexity:    O(distinct(`nNums`))
+ *
+ * References:
+ *  https://leetcode.com/problems/array-of-doubled-pairs/discuss/203183/JavaC%2B%2BPython-Match-from-the-Smallest-or-Biggest-100
+ */
+package com.zea7ot.leetcode.lvl4.lc0954
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+import java.util.*
+
+@Suppress(UNUSED)
+class SolutionApproach0TreeMap1 {
+    fun canReorderDoubled(nums: IntArray): Boolean {
+        // not used
+        // val nNums = nums.size
+
+        if (nums.sum() % 3 != 0) return false
+
+        val freqs = TreeMap<Int, Int>()
+        for (num in nums) freqs[num] = 1 + (freqs[num] ?: 0)
+
+        for ((num, freq) in freqs) {
+            if (freq == 0) continue
+
+            val expected = if (num < 0) num / 2 else num * 2
+            if (num < 0 && num % 2 != 0 || freq > (freqs[expected] ?: 0)) return false
+
+            freqs[expected] = (freqs[expected] ?: 0) - freq
+        }
+
+        return true
+    }
+}
