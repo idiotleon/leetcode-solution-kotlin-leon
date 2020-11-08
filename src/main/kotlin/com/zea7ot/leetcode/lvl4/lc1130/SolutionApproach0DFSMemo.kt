216990bc9bbfa0845adfbf91b389a337900a2d3e
Leon <yanglyu.leon.7@gmail.com>
2020-11-07 21:42:51 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc1130/SolutionApproach0DFSMemo.kt
@@ -0,0 +1,42 @@
+/**
+ * https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/
+ *
+ * Time Complexity:     O(`nNums` ^ 3)
+ * Space Complexity:    O(`nNums` ^ 2)
+ *
+ * References:
+ *  https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/discuss/398420/Java-DP-Solution-very-easy-to-understand
+ */
+package com.zea7ot.leetcode.lvl4.lc1130
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0DFSMemo {
+    fun mctFromLeafValues(nums: IntArray): Int {
+        val nNums = nums.size
+        val memo = Array(nNums) { Array<Int?>(nNums) { null } }
+        return dfs(0, nNums - 1, nums, memo)
+    }
+
+    private fun dfs(lo: Int, hi: Int, nums: IntArray, memo: Array<Array<Int?>>): Int {
+        if (lo == hi) return 0
+        memo[lo][hi]?.let { return it }
+
+        var minCost = Int.MAX_VALUE
+        for (i in lo until hi) {
+            val left = dfs(lo, i, nums, memo)
+            val right = dfs(i + 1, hi, nums, memo)
+
+            var maxLeft = 0
+            var maxRight = 0
+
+            for (j in lo..i) maxLeft = maxOf(maxLeft, nums[j])
+            for (j in i + 1..hi) maxRight = maxOf(maxRight, nums[j])
+            minCost = minOf(minCost, left + right + maxLeft * maxRight)
+        }
+
+        memo[lo][hi] = minCost
+        return minCost
+    }
+}
---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc1130/SolutionApproach1DP2Dimen.kt
@@ -0,0 +1,47 @@
+/**
+ * https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/
+ *
+ * Time Complexity:     O(`nNums` ^ 3)
+ * Space Complexity:    O(`nNums` ^ 2)
+ *
+ * dp[i, j] = dp[i, k] + dp[k + 1, j] + max(nums[i, k]) * max(nums[k + 1, j])
+ *
+ * References:
+ *  https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/discuss/340027/Java-DP-easy-to-understand
+ *  https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/discuss/340489/Summary-and-reasoning-of-different-solutions
+ */
+package com.zea7ot.leetcode.lvl4.lc1130
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach1DP2Dimen {
+    fun mctFromLeafValues(nums: IntArray): Int {
+        val nNums = nums.size
+        val dp = Array(nNums) { IntArray(nNums) { 0 } }
+
+        for (level in 2..nNums) {
+            for (lo in 0..nNums - level) {
+                val hi = lo + level - 1
+
+                for (k in lo until hi) {
+                    dp[lo][hi] = if (dp[lo][hi] != 0) {
+                        minOf(dp[lo][hi], dp[lo][k] + dp[k + 1][hi] + (getMax(lo, k, nums) * getMax(k + 1, hi, nums)))
+                    } else {
+                        dp[lo][k] + dp[k + 1][hi] + (getMax(lo, k, nums) * getMax(k + 1, hi, nums))
+                    }
+                }
+            }
+        }
+
+        return dp[0][nNums - 1]
+    }
+
+    private fun getMax(lo: Int, hi: Int, nums: IntArray): Int {
+        var max = Int.MIN_VALUE
+        for (k in lo..hi) {
+            max = maxOf(max, nums[k])
+        }
+        return max
+    }
+}
