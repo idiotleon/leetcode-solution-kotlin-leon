0656d7be148621b974f96243db5a358076695a9c
Leon <yanglyu.leon.7@gmail.com>
2020-11-08 16:09:06 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0128/SolutionApproach0HashMap.kt
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ *
+ * Time Complexity:     O(`nNums`)
+ * Space Complexity:    O(`nNums`)
+ *
+ * References:
+ *  https://leetcode.com/problems/longest-consecutive-sequence/discuss/41055/My-really-simple-Java-O(n)-solution-Accepted/39096
+ *  https://leetcode.com/problems/longest-consecutive-sequence/discuss/41055/My-really-simple-Java-O(n)-solution-Accepted
+ */
+package com.zea7ot.leetcode.lvl4.lc0128
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0HashMap {
+    fun longestConsecutive(nums: IntArray): Int {
+        // not used
+        // val nNums = nums.size
+
+        var longest = 0
+
+        val rangeMap = HashMap<Int, Int>()
+        for (num in nums) {
+            if (rangeMap.containsKey(num)) continue
+
+            val lo = rangeMap[num - 1] ?: 0
+            val hi = rangeMap[num + 1] ?: 0
+            val len = lo + hi + 1
+
+            rangeMap[num] = len
+            longest = maxOf(longest, len)
+
+            if (lo > 0) rangeMap[num - lo] = len
+            if (hi > 0) rangeMap[num + hi] = len
+        }
+
+        return longest
+    }
+}
---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc1562/SolutionApproach0Greedy.kt
@@ -0,0 +1,46 @@
+/**
+ * https://leetcode.com/problems/find-latest-group-of-size-m/
+ *
+ * Time Complexity:     O(`nNums`)
+ * Space Complexity:    O(1)
+ *
+ * References:
+ *  https://leetcode.com/problems/find-latest-group-of-size-m/discuss/806786/JavaC++Python-Count-the-Length-of-Groups-O(N)/667897
+ *  https://leetcode.com/problems/find-latest-group-of-size-m/discuss/806786/JavaC++Python-Count-the-Length-of-Groups-O(N)/669065
+ *  https://leetcode.com/problems/find-latest-group-of-size-m/discuss/806786/JavaC%2B%2BPython-Count-the-Length-of-Groups-O(N)
+ */
+package com.zea7ot.leetcode.lvl4.lc1562
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0Greedy {
+    fun findLatestStep(nums: IntArray, m: Int): Int {
+        val nNums = nums.size
+        var latest = -1
+
+        val binaryString = IntArray(nNums + 2) { 0 }
+        var count = 0
+
+        for (idx in nums.indices) {
+            // the index in the `binaryString`
+            val idxBS = nums[idx]
+            val lo = binaryString[idxBS - 1]
+            val hi = binaryString[idxBS + 1]
+            val total = lo + hi + 1
+
+            if (lo == m) --count
+            if (hi == m) --count
+
+            if (total == m) ++count
+
+            if (count > 0) latest = idx + 1
+
+            binaryString[idxBS] = total
+            binaryString[idxBS - lo] = total
+            binaryString[idxBS + hi] = total
+        }
+
+        return latest
+    }
+}
