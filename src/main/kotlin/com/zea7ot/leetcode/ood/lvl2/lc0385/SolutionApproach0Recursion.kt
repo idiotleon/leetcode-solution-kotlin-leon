4779f958599bae1efc5f7e19415acf5ab233e021
Leon <yanglyu.leon.7@gmail.com>
2020-11-08 17:30:43 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/ood/lvl2/lc0385/SolutionApproach0Recursion.kt
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/mini-parser/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/mini-parser/discuss/86083/Short-Java-recursive-solution
+ */
+package com.zea7ot.leetcode.ood.lvl2.lc0385
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0Recursion {
+}
---
+++ src/main/kotlin/com/zea7ot/leetcode/ood/lvl2/lc0385/SolutionApproach0Stack.kt
@@ -0,0 +1,60 @@
+/**
+ * https://leetcode.com/problems/mini-parser/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/mini-parser/discuss/86066/An-Java-Iterative-Solution/91186
+ *  https://leetcode.com/problems/mini-parser/discuss/86066/An-Java-Iterative-Solution
+ */
+package com.zea7ot.leetcode.ood.lvl2.lc0385
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+import java.util.*
+
+@Suppress(UNUSED)
+class SolutionApproach0Stack {
+    private companion object {
+        private const val OPEN_PAREN = '['
+        private const val CLOSED_PAREN = ']'
+        private const val COMMA = ','
+    }
+
+    fun deserialize(str: String): FakeNestedInteger {
+        val lenS = str.length
+
+        if (!str.startsWith(OPEN_PAREN)) return FakeNestedInteger(str.toInt())
+
+        val ans = FakeNestedInteger()
+        val stack = LinkedList<FakeNestedInteger>().also { it.push(ans) }
+        var start = 1
+
+        for (idx in 1 until lenS) {
+            when (val ch = str[idx]) {
+                OPEN_PAREN -> {
+                    val ni = FakeNestedInteger()
+                    stack.peek().add(ni)
+                    stack.push(ni)
+                    start = idx + 1
+                }
+
+                CLOSED_PAREN, COMMA -> {
+                    if (idx > start) {
+                        val value = str.substring(start, idx).toInt()
+                        stack.peek().add(FakeNestedInteger(value))
+                    }
+
+                    start = idx + 1
+                    if (ch == CLOSED_PAREN) stack.pop()
+                }
+
+                else -> {
+
+                }
+            }
+        }
+
+        return ans
+    }
+}
