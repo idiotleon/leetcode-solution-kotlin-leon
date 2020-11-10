13ca969cc988d657a88844cc11b3d6965d2ba34f
Leon <yanglyu.leon.7@gmail.com>
2020-11-10 09:37:10 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc1125/withBit/SolutionApproach0DFSMemo.kt
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/smallest-sufficient-team/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/smallest-sufficient-team/discuss/334669/Java-DFS-search-with-Pruning-using-Bitwise-tricks-4ms-beating-98/307521
+ *  https://leetcode.com/problems/smallest-sufficient-team/discuss/334669/Java-DFS-search-with-Pruning-using-Bitwise-tricks-4ms-beating-98
+ */
+package com.zea7ot.leetcode.lvl4.lc1125.withBit
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0DFSMemo {
+}
