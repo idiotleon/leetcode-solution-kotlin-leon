f5e90e33594f0acbebef23c95c4ccc5083c20828
Leon <yanglyu.leon.7@gmail.com>
2020-11-16 23:43:59 -0800

    reviews

--- src/main/kotlin/com/zea7ot/leetcode/lvl3/lc0712/SolutionApproach0DP2Dimen.kt
+++ src/main/kotlin/com/zea7ot/leetcode/lvl3/lc0712/SolutionApproach0DP2Dimen.kt
@@ -22,20 +22,23 @@ class SolutionApproach0DP2Dimen {
         val len2 = s2.length
 
         val dp = Array(len1 + 1) { IntArray(len2 + 1) { 0 } }
+        // to initialize the 1st col with ASCII values of `s1`
         for (idx1 in 1..len1) {
             dp[idx1][0] = dp[idx1 - 1][0] + s1[idx1 - 1].toInt()
         }
 
+        // to initialize the 1st row with ASCII values of `s2`
         for (idx2 in 1..len2) {
             dp[0][idx2] = dp[0][idx2 - 1] + s2[idx2 - 1].toInt()
         }
 
         for (idx1 in 1..len1) {
             for (idx2 in 1..len2) {
-                if (s1[idx1 - 1] == s2[idx2 - 1]) {
-                    dp[idx1][idx2] = dp[idx1 - 1][idx2 - 1]
+                dp[idx1][idx2] = if (s1[idx1 - 1] == s2[idx2 - 1]) {
+                    dp[idx1 - 1][idx2 - 1]
                 } else {
-                    dp[idx1][idx2] = minOf(dp[idx1 - 1][idx2] + s1[idx1 - 1].toInt(), dp[idx1][idx2 - 1] + s2[idx2 - 1].toInt())
+                    minOf(dp[idx1 - 1][idx2] + s1[idx1 - 1].toInt(),
+                            dp[idx1][idx2 - 1] + s2[idx2 - 1].toInt())
                 }
             }
         }
