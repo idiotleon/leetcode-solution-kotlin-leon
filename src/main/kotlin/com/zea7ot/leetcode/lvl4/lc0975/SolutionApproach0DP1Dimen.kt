5a6727e2197ac7b41d0946fc02e241093ab0fce7
Leon <yanglyu.leon.7@gmail.com>
2020-11-20 20:42:21 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0975/SolutionApproach0DP1Dimen.kt
@@ -0,0 +1,46 @@
+/**
+ * https://leetcode.com/problems/odd-even-jump/
+ *
+ * Time Complexity:     O(`nJumps` * lg(`nJumps`))
+ * Space Complexity:    O(`nJumps`)
+ *
+ * References:
+ *  https://leetcode.com/problems/odd-even-jump/discuss/217981/JavaC%2B%2BPython-DP-using-Map-or-Stack
+ *  https://leetcode.com/problems/odd-even-jump/discuss/419765/And-another-Java-TreeMap-solution-with-explanation
+ */
+package com.zea7ot.leetcode.lvl4.lc0975
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+import java.util.*
+
+@Suppress(UNUSED)
+class SolutionApproach0DP1Dimen {
+    fun oddEvenJumps(jumps: IntArray): Int {
+        val nJumps = jumps.size
+
+        val higher = BooleanArray(nJumps) { idx -> idx == nJumps - 1 }
+        val lower = BooleanArray(nJumps) { idx -> idx == nJumps - 1 }
+
+        val map = TreeMap<Int, Int>()
+        map[jumps.last()] = nJumps - 1
+
+        var count = 1
+
+        for (idx in nJumps - 2 downTo 0) {
+            map.floorEntry(jumps[idx])?.let {
+                val (_, nextSmaller) = it
+                lower[idx] = higher[nextSmaller]
+            }
+
+            map.ceilingEntry(jumps[idx])?.let {
+                val (_, nextGreater) = it
+                higher[idx] = lower[nextGreater]
+            }
+
+            if (higher[idx]) ++count
+            map[jumps[idx]] = idx
+        }
+
+        return count
+    }
+}
---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0975/SolutionApproach0DP2Dimen.kt
@@ -0,0 +1,46 @@
+/**
+ * https://leetcode.com/problems/odd-even-jump/
+ *
+ * Time Complexity:     O(`nJumps` * lg(`nJumps`))
+ * Space Complexity:    O(`nJumps`)
+ *
+ * References:
+ *  https://leetcode.com/problems/odd-even-jump/discuss/217974/Java-solution-DP-%2B-TreeMap
+ */
+package com.zea7ot.leetcode.lvl4.lc0975
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+import java.util.*
+
+@Suppress(UNUSED)
+class SolutionApproach0DP2Dimen {
+    fun oddEvenJumps(jumps: IntArray): Int {
+        val nJumps = jumps.size
+        val dp = Array(nJumps) { idxJump -> BooleanArray(2) { idxJump == nJumps - 1 } }
+
+        val map = TreeMap<Int, Int>()
+        map[jumps.last()] = nJumps - 1
+
+        var count = 1
+
+        for (idx in nJumps - 2 downTo 0) {
+            // odd jump
+            map.ceilingEntry(jumps[idx])?.let {
+                val (_, nextGreater) = it
+                dp[idx][0] = dp[nextGreater][1]
+            }
+
+            // even jump
+            map.floorEntry(jumps[idx])?.let {
+                val (_, nextSmaller) = it
+                dp[idx][1] = dp[nextSmaller][0]
+            }
+
+            map[jumps[idx]] = idx
+
+            count += if (dp[idx][0]) 1 else 0
+        }
+
+        return count
+    }
+}
