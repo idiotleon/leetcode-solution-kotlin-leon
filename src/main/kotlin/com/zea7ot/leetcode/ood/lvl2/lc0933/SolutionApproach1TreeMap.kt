c405c988c430d2207495ef976ba1538f62c24fbf
Leon <yanglyu.leon.7@gmail.com>
2020-11-25 22:16:25 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/ood/lvl2/lc0933/SolutionApproach1TreeMap.kt
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/number-of-recent-calls/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/number-of-recent-calls/discuss/189239/JavaPython-3-Five-solutions%3A-TreeMap-TreeSet-ArrayList-Queue-Circular-List.
+ */
+package com.zea7ot.leetcode.ood.lvl2.lc0933
+
+import com.zea7ot.leetcode.utils.Constant.Annotation.Companion.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach1TreeMap {
+}
