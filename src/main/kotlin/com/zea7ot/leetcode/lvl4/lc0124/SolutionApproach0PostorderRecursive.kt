f9829d0f84e9f8c530c0bd5dc46b74a7aee54018
Leon <yanglyu.leon.7@gmail.com>
2020-12-13 08:54:40 -0800

    reviews

--- src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0124/SolutionApproach0PostorderRecursive.kt
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0124/SolutionApproach0PostorderRecursive.kt
@@ -15,23 +15,22 @@ import com.zea7ot.leetcode.util.Constant.Annotation.Companion.UNUSED
 import com.zea7ot.leetcode.util.dataStructure.tree.TreeNode
 
 @Suppress(UNUSED)
-class SolutionApproach0DFSRecursive {
+class SolutionApproach0PostorderRecursive {
     fun maxPathSum(root: TreeNode?): Int {
         val maxSum = intArrayOf(Int.MIN_VALUE)
-        dfs(root, maxSum)
+        postorder(root, maxSum)
         return maxSum[0]
     }
 
-    private fun dfs(node: TreeNode?, maxSum: IntArray): Int {
+    private fun postorder(node: TreeNode?, maxSum: IntArray): Int {
         if (node == null) return 0
 
+        val leftPathSum = postorder(node.left, maxSum)
+        val rightPathSum = postorder(node.right, maxSum)
+
         val value = node.`val`
+        maxSum[0] = maxOf(maxSum[0], leftPathSum + rightPathSum + value)
 
-        val left = dfs(node.left, maxSum)
-        val right = dfs(node.right, maxSum)
-
-        maxSum[0] = maxOf(maxSum[0], left + right + value)
-
-        return maxOf(0, maxOf(left, right) + value)
+        return maxOf(0, maxOf(leftPathSum, rightPathSum) + value)
     }
 }
---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0124/SolutionApproach0PostorderRecursive1.kt
@@ -0,0 +1,34 @@
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/
+ *
+ * Time Complexity:     O(H)
+ * Space Complexity:    O(H)
+ */
+package com.zea7ot.leetcode.lvl4.lc0124
+
+import com.zea7ot.leetcode.util.Constant.Annotation.Companion.UNUSED
+import com.zea7ot.leetcode.util.dataStructure.tree.TreeNode
+
+@Suppress(UNUSED)
+class SolutionApproach0PostorderRecursive1 {
+    private var maxPathSum = Int.MIN_VALUE
+
+    fun maxPathSum(root: TreeNode?): Int {
+        postorder(root)
+        return maxPathSum
+    }
+
+    private fun postorder(node: TreeNode?): Int {
+        if (node == null) return 0
+
+        val leftPathSum = postorder(node.left)
+        val rightPathSum = postorder(node.right)
+
+        val value = node.`val`
+        val sum = leftPathSum + rightPathSum + value
+        maxPathSum = maxOf(maxPathSum, sum)
+
+        return maxOf(0, value + leftPathSum, value + rightPathSum)
+    }
+}
