cacd36f1775d35181c6dfbb7d7fa3b7cb0c8f6fe
Leon <yanglyu.leon.7@gmail.com>
2020-12-15 15:34:14 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl2/lc0369/SolutionApproach0DFSRecursive.kt
@@ -0,0 +1,36 @@
+/**
+ * https://leetcode.com/problems/plus-one-linked-list/
+ *
+ * Time Complexity:     O(L)
+ * Space Complexity:    O(1)
+ *
+ * References:
+ *  https://leetcode.com/problems/plus-one-linked-list/discuss/84130/Java-recursive-solution
+ */
+package com.zea7ot.leetcode.lvl2.lc0369
+
+import com.zea7ot.leetcode.util.Constant.Annotation.Companion.UNUSED
+import com.zea7ot.leetcode.util.dataStructure.linkedList.ListNode
+
+@Suppress(UNUSED)
+class SolutionApproach0DFSRecursive {
+    fun plusOne(head: ListNode?): ListNode? {
+        if (dfs(head) == 0) {
+            return head
+        }
+        
+        val newHead = ListNode(1)
+        newHead.next = head
+        return newHead
+    }
+
+    private fun dfs(head: ListNode?): Int {
+        if (head == null) return 1
+
+        val carry = dfs(head.next)
+
+        val value = carry + head.`val`
+        head.`val` = value % 10
+        return value / 10
+    }
+}
