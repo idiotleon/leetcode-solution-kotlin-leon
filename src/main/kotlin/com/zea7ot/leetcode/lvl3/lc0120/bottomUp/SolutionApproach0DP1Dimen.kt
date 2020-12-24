871d80603f51fee83d3a7531c13013a1d6e4635d
Leon <yanglyu.leon.7@gmail.com>
2020-12-24 13:58:03 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl3/lc0120/bottomUp/SolutionApproach0DP1Dimen.kt
@@ -0,0 +1,33 @@
+/**
+ * https://leetcode.com/problems/triangle/
+ *
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(`nRows`)
+ *
+ * a bottom-up approach to dissemble the problem into subproblems *
+ *
+ * References:
+ *  https://leetcode.com/problems/triangle/discuss/38724/7-lines-neat-Java-Solution
+ *  https://leetcode.com/problems/triangle/discuss/38732/My-8-line-DP-Java-code(4-meaningful-lines)-with-O(1)-space
+ */
+package com.zea7ot.leetcode.lvl3.lc0120.bottomUp
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0DP1Dimen {
+    fun minimumTotal(triangle: List<List<Int>>): Int {
+        val nRows = triangle.size
+        val dp = IntArray(1 + nRows) { 0 }
+
+        for (row in triangle.indices.reversed()) {
+            val curRow = triangle[row]
+
+            for (col in curRow.indices) {
+                dp[col] = minOf(dp[col], dp[col + 1]) + curRow[col]
+            }
+        }
+
+        return dp[0]
+    }
+}
---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl3/lc0120/topDown/SolutionApproach1DFSNaive.kt
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode.com/problems/triangle/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * TLEed
+ *
+ * References:
+ *  https://leetcode.com/problems/triangle/discuss/159686/Java-Recursive-greaterTop-Down-greater-Bottom-up-greater-Bottom-Up-%2B-Optimal-Space
+ */
+package com.zea7ot.leetcode.lvl3.lc0120.topDown
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach1DFSNaive {
+    fun minimumTotal(triangle: List<List<Int>>) = dfs(0, 0, triangle)
+
+    private fun dfs(row: Int, col: Int, triangle: List<List<Int>>): Int {
+        val nRows = triangle.size
+        if (row + 1 >= nRows) {
+            return triangle[row][col]
+        }
+
+        return triangle[row][col] + minOf(
+            dfs(1 + row, col, triangle),
+            dfs(1 + row, 1 + col, triangle)
+        )
+    }
+}
