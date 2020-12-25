bc8825e6762fa92d50da6851cf98114642b0d811
Leon <yanglyu.leon.7@gmail.com>
2020-12-24 18:22:06 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/summary/byDataStructure/stack/arrayImplementation/monotonicStack/StrictlyDecreasingMonoStack.kt
@@ -0,0 +1,37 @@
+/**
+ * @author: Leon
+ *
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(N)
+ *
+ * References:
+ *  Paid Course, 87min: https://www.acwing.com/video/15/
+ */
+package com.zea7ot.summary.byDataStructure.stack.arrayImplementation.monotonicStack
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class StrictlyDecreasingMonoStack {
+    private companion object {
+        private const val DATA_RANGE = 1e6.toInt() + 7
+    }
+
+    private val stack = Array<Int?>(DATA_RANGE) { null }
+    private var idx = 0
+
+    fun main() {
+        // number of inputs
+        val nInputs = readLine()!!.toInt()
+
+        for (input in 0 until nInputs) {
+            // to read the input(s)
+            val value = readLine()!!.toInt()
+
+            while (idx > 0 && stack[idx]!! >= value) --idx
+
+            stack[idx] = value
+            ++idx
+        }
+    }
+}
