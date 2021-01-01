070b9ce300f13885b1d82dd67fdad838b60cb4c0
Leon <yanglyu.leon.7@gmail.com>
2020-12-31 20:54:21 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl5/lc0493/SolutionApproach0SegmentTree.kt
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/reverse-pairs/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/reverse-pairs/discuss/298986/Java-segment-tree
+ */
+package com.zea7ot.leetcode.lvl5.lc0493
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0SegmentTree {
+}
