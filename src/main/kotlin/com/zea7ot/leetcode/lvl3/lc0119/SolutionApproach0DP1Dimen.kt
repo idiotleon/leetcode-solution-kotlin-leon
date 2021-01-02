59ac42bd674ba8e7af1af13b6aba5570faa4e2e0
Leon <yanglyu.leon.7@gmail.com>
2021-01-01 21:16:38 -0800

    reviews

--- src/main/kotlin/com/zea7ot/leetcode/lvl3/lc0119/SolutionApproach0DP1Dimen.kt
+++ src/main/kotlin/com/zea7ot/leetcode/lvl3/lc0119/SolutionApproach0DP1Dimen.kt
@@ -16,16 +16,16 @@ import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
 class SolutionApproach0DP1Dimen {
     fun getRow(rowIndex: Int): List<Int> {
         // sanity check
-        if (rowIndex < 0) return arrayListOf()
+        if (rowIndex < 0) return mutableListOf()
 
-        val dp = IntArray(rowIndex + 1) { 1 }
+        val dp = MutableList(rowIndex + 1) { 1 }
 
         for (row in 1..rowIndex) {
             for (col in row - 1 downTo 1) {
                 dp[col] = dp[col] + dp[col - 1]
             }
         }
-
-        return dp.toList()
+        
+        return dp
     }
 }
