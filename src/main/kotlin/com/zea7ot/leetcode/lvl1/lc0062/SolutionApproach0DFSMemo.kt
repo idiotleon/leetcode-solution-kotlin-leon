469c0f025d8ea8cb9f65a751598912d97c7aa586
Leon <yanglyu.leon.7@gmail.com>
2021-01-01 23:59:25 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl1/lc0062/SolutionApproach0DFSMemo.kt
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/problems/unique-paths/
+ *
+ * Time Complexity:     O(`m` * `n`)
+ * Space Complexity:    O(`m` * `n`)
+ *
+ * References:
+ *  https://youtu.be/BxblkIz6TZc?t=518
+ */
+package com.zea7ot.leetcode.lvl1.lc0062
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0DFSMemo {
+    fun uniquePaths(m: Int, n: Int): Int {
+        val memo = Array(m) { Array<Int?>(n) { null } }
+        return dfs(m - 1, n - 1, memo)
+    }
+
+    private fun dfs(row: Int, col: Int, memo: Array<Array<Int?>>): Int {
+        if (row < 0 || col < 0) return 0
+        if (row == 0 && col == 0) return 1
+        memo[row][col]?.let { return it }
+        val cntUniquePaths = dfs(row - 1, col, memo) + dfs(row, col - 1, memo)
+        memo[row][col] = cntUniquePaths
+        return cntUniquePaths
+    }
+}
