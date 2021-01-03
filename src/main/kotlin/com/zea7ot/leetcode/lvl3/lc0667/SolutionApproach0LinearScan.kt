a056a5e0b9285bd583cbec7300b100922c9a2d21
Leon <yanglyu.leon.7@gmail.com>
2021-01-03 15:29:50 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl3/lc0667/SolutionApproach0LinearScan.kt
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/beautiful-arrangement-ii/
+ *
+ * Time Complexity:     O(`n`)
+ * Space Complexity:    O(`n`) / O(1)
+ *
+ * References:
+ *  https://leetcode.com/problems/beautiful-arrangement-ii/discuss/106948/C++-Java-Clean-Code-4-liner/109226
+ *  https://leetcode.com/problems/beautiful-arrangement-ii/discuss/106948/C%2B%2B-Java-Clean-Code-4-liner
+ */
+package com.zea7ot.leetcode.lvl3.lc0667
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0LinearScan {
+    fun constructArray(n: Int, k: Int): IntArray {
+        val ans = IntArray(n) { 0 }
+
+        var lo = 1
+        var hi = n
+        var k = k
+
+        for (idx in 0 until n) {
+            ans[idx] = if (k % 2 == 0) {
+                hi--
+            } else {
+                lo++
+            }
+
+            if (k > 1) {
+                --k
+            }
+        }
+
+        return ans
+    }
+}
