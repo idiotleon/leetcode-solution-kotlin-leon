ae2ad1f59f3ac93d791c1df0bcdc696959c67324
Leon <yanglyu.leon.7@gmail.com>
2021-01-03 23:21:44 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc1692/SolutionApproach0DFSMemo.kt
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/count-ways-to-distribute-candies/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/count-ways-to-distribute-candies/discuss/976136/Bottom-Up-Top-Down-Linear-space-JavaPython
+ */
+package com.zea7ot.leetcode.lvl4.lc1692
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0DFSMemo {
+}
---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc1692/SolutionApproach0DP2Dimen.kt
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode.com/problems/count-ways-to-distribute-candies/
+ *
+ * Time Complexity:     O(`n` * `k`)
+ * Space Complexity:    O(`n` * `k`)
+ *
+ * dp[i][j], to use all the first i candies to put them into j bags
+ *
+ * References:
+ *  https://leetcode.com/problems/count-ways-to-distribute-candies/discuss/979889/Java-Concise-DP-Solution-with-Explanations
+ */
+package com.zea7ot.leetcode.lvl4.lc1692
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0DP2Dimen {
+    private companion object {
+        private const val MOD = 1e9.toLong() + 7
+    }
+
+    fun waysToDistribute(n: Int, k: Int): Int {
+        val dp = Array(n + 1) { LongArray(k + 1) { 0L } }.also {
+            it[0][0] = 1
+        }
+
+        for (idxBag in 1..k) {
+            for (idxCandy in idxBag..n) {
+                // the `idxCandy` candy to be put into a standalone bag
+                dp[idxCandy][idxBag] = dp[idxCandy - 1][idxBag - 1]
+
+                // to put the `idxCandy` candy into the previous `idxBag` bags
+                if (idxCandy - 1 >= idxBag) dp[idxCandy][idxBag] += dp[idxCandy - 1][idxBag] * idxBag % MOD
+                dp[idxCandy][idxBag] %= MOD
+            }
+        }
+
+        return dp[n][k].toInt()
+    }
+}
