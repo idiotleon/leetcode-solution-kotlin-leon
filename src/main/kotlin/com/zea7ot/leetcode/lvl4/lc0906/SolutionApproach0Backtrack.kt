1cab9e44d0f6f8968c31fe6299e7b1302ce212d6
Leon <yanglyu.leon.7@gmail.com>
2021-01-04 23:55:34 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0906/SolutionApproach0Backtrack.kt
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/super-palindromes/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/super-palindromes/discuss/171467/c%2B%2B-straightforward-backtracking-solution
+ */
+package com.zea7ot.leetcode.lvl4.lc0906
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0Backtrack {
+}
