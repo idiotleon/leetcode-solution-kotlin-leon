82ca7feea9ae17181a3253008ef17af11a3cb688
Leon <yanglyu.leon.7@gmail.com>
2021-01-05 17:27:56 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc0330/SolutionApproach0Greedy.kt
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/patching-array/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/patching-array/discuss/78492/C%2B%2B-8ms-greedy-solution-with-explanation
+ *  https://leetcode.com/problems/patching-array/discuss/280183/Detailed-Explanation-with-Example
+ */
+package com.zea7ot.leetcode.lvl4.lc0330
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0Greedy {
+    fun minPatches(nums: IntArray, n: Int): Int {
+        val nNums = nums.size
+
+        // the largest number that currently can be achieved
+        var patch = 0L
+        var count = 0
+        var idx = 0
+
+        while (patch < n) {
+            if (idx < nNums && patch + 1 >= nums[idx]) {
+                patch += nums[idx].toLong()
+                ++idx
+            } else {
+                patch += (patch + 1)
+                ++count
+            }
+        }
+
+        return count
+    }
+}
