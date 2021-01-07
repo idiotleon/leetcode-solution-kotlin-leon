eb8985a99eb509918523b717496ba53cc84c9833
Leon <yanglyu.leon.7@gmail.com>
2021-01-06 23:00:21 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc1655/SolutionApproach0Backtrack.kt
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/distribute-repeating-integers/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/distribute-repeating-integers/discuss/935428/Java-Backtrack-solution-got-accepted-with-88-ms
+ *  https://leetcode.com/problems/distribute-repeating-integers/discuss/935474/c%2B%2B-DFS-Backtrack-solution
+ */
+package com.zea7ot.leetcode.lvl4.lc1655
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0Backtrack {
+}
---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl4/lc1655/SolutionApproach0DP2Dimen.kt
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/distribute-repeating-integers/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/distribute-repeating-integers/discuss/935824/C%2B%2B-State-Compression-DP-O(50*3m)
+ *  https://leetcode.com/problems/distribute-repeating-integers/discuss/935522/Step-by-step-optimization-more-than-10-methods
+ */
+package com.zea7ot.leetcode.lvl4.lc1655
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0DP2Dimen {
+}
