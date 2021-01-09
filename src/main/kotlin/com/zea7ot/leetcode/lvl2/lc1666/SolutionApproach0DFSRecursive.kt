599b52cacc5a4af7371e29519525ff7b33572724
Leon <yanglyu.leon.7@gmail.com>
2021-01-08 19:58:33 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl2/lc1666/SolutionApproach0DFSRecursive.kt
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/change-the-root-of-a-binary-tree/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/change-the-root-of-a-binary-tree/discuss/1005545/Java-Simple-Recursion
+ */
+package com.zea7ot.leetcode.lvl2.lc1666
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0DFSRecursive {
+}
