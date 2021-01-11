9a28558473316bcd5a3e294508a9c6eaf255e911
Leon <yanglyu.leon.7@gmail.com>
2021-01-10 19:37:33 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/lvl5/lc0411/SolutionApproach0Trie.kt
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/minimum-unique-word-abbreviation/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/minimum-unique-word-abbreviation/discuss/89885/Trie-%2B-Bruteforce
+ *  https://leetcode.com/problems/minimum-unique-word-abbreviation/discuss/89883/Java-DFS%2BTrie%2BBinary-Search-90ms
+ */
+package com.zea7ot.leetcode.lvl5.lc0411
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0Trie {
+}
