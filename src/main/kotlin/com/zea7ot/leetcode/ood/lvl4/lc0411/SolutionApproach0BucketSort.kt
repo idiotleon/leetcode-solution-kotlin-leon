0f4a8694f722a5b2400ad50c76668c9ea005728a
Leon <yanglyu.leon.7@gmail.com>
2021-01-10 20:16:44 -0800

    reviews

---
+++ src/main/kotlin/com/zea7ot/leetcode/ood/lvl4/lc0411/SolutionApproach0BucketSort.kt
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode.com/problems/online-majority-element-in-subarray/
+ *
+ * Time Complexities:
+ *  initialization:     O()
+ *  `query`:            O()
+ *
+ * Space Complexity:    O()
+ *
+ * References:
+ *  https://leetcode.com/problems/online-majority-element-in-subarray/discuss/356227/C++-Codes-of-different-approaches-(Random-Pick-Trade-off-Segment-Tree-Bucket)/336843
+ *  https://leetcode.com/problems/online-majority-element-in-subarray/discuss/356227/C%2B%2B-Codes-of-different-approaches-(Random-Pick-Trade-off-Segment-Tree-Bucket)
+ */
+package com.zea7ot.leetcode.ood.lvl4.lc0411
+
+import com.zea7ot.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0BucketSort {
+}
