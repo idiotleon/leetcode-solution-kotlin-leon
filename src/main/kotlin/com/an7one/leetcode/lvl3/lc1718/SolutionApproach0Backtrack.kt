1ebf1f33bcff71807d5a3c35e4ca60b5049bfad4
Leon <yanglyu.leon.7@gmail.com>
2021-06-14 18:03:56 -0700

    review

---
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc1718/SolutionApproach0Backtrack.kt
@@ -0,0 +1,57 @@
+/**
+ * https://leetcode.com/problems/construct-the-lexicographically-largest-valid-sequence/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * Reference:
+ *  https://leetcode.com/problems/construct-the-lexicographically-largest-valid-sequence/discuss/1008864/Java-or-C%2B%2Bor-Detailed-explanation-or-BackTracking-or-100-faster
+ *  https://leetcode.com/problems/construct-the-lexicographically-largest-valid-sequence/discuss/1008913/C%2B%2B-Backtracking
+ */
+package com.an7one.leetcode.lvl3.lc1718
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0Backtrack {
+    fun constructDistancedSequence(n: Int): IntArray {
+        val ans = IntArray(2 * n - 1)
+        val used = BooleanArray(n + 1) { false }
+
+        backtrack(0, used, n, ans)
+
+        return ans
+    }
+
+    private fun backtrack(idxStart: Int, used: BooleanArray, n: Int, res: IntArray): Boolean {
+        val len = res.size
+        if (idxStart == len)
+            return true
+
+        if (res[idxStart] != 0)
+            return backtrack(idxStart + 1, used, n, res)
+
+        for (idx in n downTo 1) {
+            if (used[idx])
+                continue
+
+            used[idx] = true
+            res[idxStart] = idx
+
+            if (idx == 1) {
+                if (backtrack(idxStart + 1, used, n, res))
+                    return true
+            } else if (idxStart + idx < len && res[idxStart + idx] == 0) {
+                res[idxStart + idx] = idx
+                if (backtrack(idxStart + 1, used, n, res))
+                    return true
+                res[idxStart + idx] = 0
+            }
+
+            res[idxStart] = 0
+            used[idx] = false
+        }
+
+        return false
+    }
+}
---
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc1868/SolutionApproach0TwoPointers1.kt
@@ -0,0 +1,74 @@
+/**
+ * https://leetcode.com/problems/product-of-two-run-length-encoded-arrays/
+ *
+ * Time Complexity:     O(`len`)
+ * Space Complexity:    O(`len`)
+ *
+ * Reference:
+ *  https://leetcode.com/problems/product-of-two-run-length-encoded-arrays/discuss/1231200/Java-two-pointer
+ */
+package com.an7one.leetcode.lvl3.lc1868
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0TwoPointers1 {
+    fun findRLEArray(encoded1: Array<IntArray>, encoded2: Array<IntArray>): List<List<Int>> {
+        val len1 = encoded1.size
+        val len2 = encoded2.size
+
+        var idx1 = 0
+        var idx2 = 0
+
+        val list = mutableListOf<List<Int>>()
+
+        while (idx1 < len1 && idx2 < len2) {
+            val (product1, freq1) = encoded1[idx1]
+            val (product2, freq2) = encoded2[idx2]
+
+            val product = product1 * product2
+
+            when {
+                freq1 == freq2 -> {
+                    list.add(listOf(product, freq1))
+                    ++idx1
+                    ++idx2
+                }
+
+                freq1 < freq2 -> {
+                    list.add(listOf(product, freq1))
+                    encoded2[idx2][1] = freq2 - freq1
+                    ++idx1
+                }
+
+                else -> {
+                    list.add(listOf(product, freq2))
+                    encoded1[idx1][1] = freq1 - freq2
+                    ++idx2
+                }
+            }
+        }
+
+        val ans = mutableListOf<List<Int>>()
+        val len = list.size
+
+        var lo = 0
+        var hi = 1
+
+        while (hi <= len) {
+            val (product, freq) = list[lo]
+            var sumFreq = freq
+            while (hi < len && product == list[hi][0]) {
+                sumFreq += list[hi][1]
+                ++hi
+            }
+
+            ans.add(listOf(product, sumFreq))
+
+            lo = hi
+            ++hi
+        }
+
+        return ans
+    }
+}
