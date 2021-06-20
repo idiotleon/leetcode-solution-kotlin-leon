21efa5ae3298de24c896b29a16ed818ba9ebf863
Leon <yanglyu.leon.7@gmail.com>
2021-06-19 22:22:33 -0700

    review

---
+++ src/main/kotlin/com/an7one/leetcode/lvl5/lc0629/SolutionApproach0DP2Dimen.kt
@@ -0,0 +1,47 @@
+/**
+ * https://leetcode.com/problems/k-inverse-pairs-array/
+ *
+ * Time Complexity:     O(`n` * `k`)
+ * Space Complexity:    O(`n` * `k`)
+ *
+ * Reference:
+ *  https://leetcode.com/problems/k-inverse-pairs-array/discuss/104815/Java-DP-O(nk)-solution
+ */
+package com.an7one.leetcode.lvl5.lc0629
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0DP2Dimen {
+    private companion object {
+        private const val MOD = 1e9.toLong() + 7
+    }
+
+    fun kInversePairs(n: Int, k: Int): Int {
+        if (k > n * (n - 1) / 2 || k < 0)
+            return 0
+
+        if (k == 0 || k == n * (n - 1) / 2)
+            return 1
+
+        val dp = Array(n + 1) { LongArray(k + 1) { 0L } }.also {
+            it[2][0] = 1L
+            it[2][1] = 1L
+        }
+
+        for (row in 3..n) {
+            dp[row][0] = 1L
+
+            for (col in 1..minOf(k, row * (row - 1) / 2)) {
+                dp[row][col] = dp[row][col - 1] + dp[row - 1][col]
+
+                if (col >= row)
+                    dp[row][col] -= dp[row - 1][col - row]
+
+                dp[row][col] = (dp[row][col] + MOD) % MOD
+            }
+        }
+
+        return dp[n][k].toInt()
+    }
+}
