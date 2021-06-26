ff24a8036fbd7f23ae537477898175f86edb9bec
Leon <yanglyu.leon.7@gmail.com>
2021-06-26 15:32:28 -0700

    refactor

--- src/main/kotlin/com/an7one/leetcode/lvl5/lc0715/SolutionApproach0SegmentTree.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl5/lc0715/SolutionApproach0SegmentTree.kt
@@ -1,11 +1,15 @@
 /**
  * https://leetcode.com/problems/range-module/
  *
- * Time Complexity:     O()
+ * Time Complexities:
+ *  addRange:       O()
+ *  queryRange:     O()
+ *  removeRange:    O()
+ *
  * Space Complexity:    O()
  *
  * References:
- *
+ *  https://leetcode.com/problems/range-module/discuss/495876/Clean-And-Concise-Lazy-Propagation-Segment-Tree
  */
 package com.an7one.leetcode.lvl5.lc0715
 
@@ -15,89 +19,74 @@ import com.an7one.leetcode.util.Constant.Annotation.UNUSED
 class SolutionApproach0SegmentTree {
     private val root = SegmentTree()
 
-    fun addRange(left: Int, right: Int) {
-        root.update(left, right - 1, 1)
-    }
+    fun addRange(left: Int, right: Int) = root.addRange(left, right)
 
-    fun queryRange(left: Int, right: Int): Boolean {
-        val freq = root.query(left, right - 1)
-        return freq > 0
-    }
+    fun queryRange(left: Int, right: Int) = root.queryRange(left, right)
 
-    fun removeRange(left: Int, right: Int) {
-        root.update(left, right - 1, 1)
-    }
+    fun removeRange(left: Int, right: Int) = root.removeRange(left, right)
 
     private class SegmentTree {
         private companion object {
             private const val RANGE = 1e9.toInt() + 1
         }
 
-        private val root = SegmentTreeNode(0, RANGE)
+        private val root = SegmentTreeNode(0, RANGE, false)
 
-        fun update(rangeLo: Int, rangeHi: Int, value: Int) = update(rangeLo, rangeHi, value, root)
-        fun query(rangeLo: Int, rangeHi: Int) = query(rangeLo, rangeHi, root)
+        fun addRange(rangeLo: Int, rangeHi: Int) = update(rangeLo, rangeHi, true, root)
+        fun removeRange(rangeLo: Int, rangeHi: Int) = update(rangeLo, rangeHi, false, root)
+        fun queryRange(rangeLo: Int, rangeHi: Int) = query(rangeLo, rangeHi, root)
 
-        private fun update(rangeLo: Int, rangeHi: Int, value: Int, node: SegmentTreeNode?) {
-            if (node == null) return
+        private fun query(rangeLo: Int, rangeHi: Int, node: SegmentTreeNode?): Boolean {
+            if (node == null)
+                return true
+
+            if (rangeLo >= node.hi || rangeHi <= node.lo)
+                return true
+
+            if (rangeLo <= node.lo && node.hi <= rangeHi || (node.left == null))
+                return node.state
+
+            val mid = node.lo + (node.hi - node.lo) / 2
+
+            return when {
+                rangeHi <= mid -> query(rangeLo, rangeHi, node.left)
+                rangeLo >= mid -> query(rangeLo, rangeHi, node.right)
+                else -> query(rangeLo, rangeHi, node.left) && query(rangeLo, rangeHi, node.right)
+            }
+        }
+
+        private fun update(
+            rangeLo: Int, rangeHi: Int,
+            value: Boolean,
+            node: SegmentTreeNode?
+        ) {
+            if (node == null)
+                return
+
             if (rangeLo <= node.lo && node.hi <= rangeHi) {
-                node.lazy += value
+                node.state = value
+                node.left = null
+                node.right = null
+                return
             }
 
-            pushDown(node)
+            if (rangeLo >= node.hi || rangeHi <= node.lo)
+                return
 
-            // complete overlap or no overlap at all
-            if (rangeLo <= node.lo && node.hi <= rangeHi || rangeHi < node.lo || rangeLo > node.hi) return
+            val mid = node.lo + (node.hi - node.lo) / 2
+            if (node.left == null) {
+                node.left = SegmentTreeNode(node.lo, mid, node.state)
+                node.right = SegmentTreeNode(mid, node.hi, node.state)
+            }
 
             update(rangeLo, rangeHi, value, node.left)
             update(rangeLo, rangeHi, value, node.right)
-
-            node.min = minOf(node.left!!.min, node.right!!.min)
-        }
-
-        private fun query(rangeLo: Int, rangeHi: Int, node: SegmentTreeNode?): Int {
-            if (node == null) return 0
-            pushDown(node)
-
-            // no overlap
-            if (rangeLo > node.hi || rangeHi < node.lo) return RANGE
-
-            // complete overlap
-            if (rangeLo <= node.lo && node.hi <= rangeHi) return node.min
-
-            val leftMin = query(rangeLo, rangeHi, node.left)
-            val rightMin = query(rangeLo, rangeHi, node.right)
-
-            return minOf(leftMin, rightMin)
-        }
-
-        private fun pushDown(node: SegmentTreeNode) {
-            node.min += node.lazy
-
-            if (node.lo != node.hi) {
-                val mid = node.lo + (node.hi - node.lo) / 2
-
-                if (node.left == null) {
-                    node.left = SegmentTreeNode(node.lo, mid)
-                }
-
-                if (node.right == null) {
-                    node.right = SegmentTreeNode(mid + 1, node.hi)
-                }
-
-
-                node.left!!.lazy += node.lazy
-                node.right!!.lazy += node.lazy
-            }
-
-            node.lazy = 0
+            node.state = node.left!!.state && node.right!!.state
         }
 
         private data class SegmentTreeNode(
-            val lo: Int,
-            val hi: Int,
-            var min: Int = 0,
-            var lazy: Int = 0,
+            val lo: Int, val hi: Int,
+            var state: Boolean = false,  // has been tracked or not
             var left: SegmentTreeNode? = null,
             var right: SegmentTreeNode? = null
         )
