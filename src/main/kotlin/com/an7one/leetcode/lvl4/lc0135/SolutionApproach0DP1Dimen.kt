5b997416a5e384f57f8dcc8ed2ccfac16a808597
Leon <yanglyu.leon.7@gmail.com>
2021-06-27 09:21:27 -0700

    review

---
+++ src/main/kotlin/com/an7one/leetcode/lvl4/lc0135/SolutionApproach0DP1Dimen.kt
@@ -0,0 +1,34 @@
+/**
+ * https://leetcode.com/problems/candy/
+ *
+ * Time Complexity:     O(`nRtgs`)
+ * Space Complexity:    O(`nRtgs`)
+ *
+ * Reference:
+ *  https://leetcode.com/problems/candy/discuss/42769/A-simple-solution/336970
+ *  https://leetcode.com/problems/candy/discuss/42769/A-simple-solution/41219
+ *  https://leetcode.com/problems/candy/discuss/42769/A-simple-solution
+ */
+package com.an7one.leetcode.lvl4.lc0135
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0DP1Dimen {
+    fun candy(ratings: IntArray): Int {
+        val nRtgs = ratings.size
+        val dp = IntArray(nRtgs) { 1 }
+
+        for (idx in 1 until nRtgs) {
+            if (ratings[idx] > ratings[idx - 1])
+                dp[idx] = dp[idx - 1] + 1
+        }
+
+        for (idx in nRtgs - 1 downTo 1) {
+            if (ratings[idx - 1] > ratings[idx])
+                dp[idx - 1] = maxOf(dp[idx] + 1, dp[idx - 1])
+        }
+
+        return dp.sum()
+    }
+}
