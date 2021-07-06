6827a490ab56b2bf5128d1adc823eebef03f4982
Leon <yanglyu.leon.7@gmail.com>
2021-07-05 22:54:00 -0700

    review

---
+++ src/main/kotlin/com/an7one/leetcode/lvl2/lc0566/SolutionApproach0MatrixManipulation.kt
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/reshape-the-matrix/
+ *
+ * Time Complexity:     O(`r` * `c`)
+ * Space Complexity:    O(`r` * `c`)
+ *
+ * Reference:
+ *  https://leetcode.com/problems/reshape-the-matrix/discuss/102491/Java-Concise-O(nm)-time
+ */
+package com.an7one.leetcode.lvl2.lc0566
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0MatrixManipulation {
+    fun matrixReshape(matrix: Array<IntArray>, r: Int, c: Int): Array<IntArray> {
+        val nRows = matrix.size
+        val nCols = matrix[0].size
+
+        if (nRows * nCols != r * c)
+            return matrix
+
+        val res = Array(r) { IntArray(c) { 0 } }
+        for (idx in 0 until r * c) {
+            res[idx / c][idx % c] = matrix[idx / nCols][idx % nCols]
+        }
+
+        return res
+    }
+}
