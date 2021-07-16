01fda538fc12e1273fe082a04d5d9a0d08d4a28e
Leon <yanglyu.leon.7@gmail.com>
2021-07-15 21:36:21 -0700

    review

---
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0611/SolutionApproach0ThreePointers.kt
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/valid-triangle-number/
+ *
+ * Time Complexity:     O(`nNums` ^ 2)
+ * Space Complexity:    O(1)
+ *
+ * Reference:
+ *  https://leetcode.com/problems/valid-triangle-number/discuss/104174/Java-O(n2)-Time-O(1)-Space
+ *  https://leetcode.com/problems/valid-triangle-number/discuss/128135/A-similar-O(n2)-solution-to-3-Sum
+ *  https://leetcode.com/problems/valid-triangle-number/discuss/321367/java-binary-search(log(n)-*-n2
+ */
+package com.an7one.leetcode.lvl3.lc0611
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
+@Suppress(UNUSED)
+class SolutionApproach0ThreePointers {
+    fun triangleNumber(nums: IntArray): Int {
+        val nNums = nums.size
+        nums.sort()
+
+        var count = 0
+        for (idx in 2 until nNums) {
+            var lo = 0
+            var hi = idx - 1
+            while (lo < hi) {
+                if (nums[lo] + nums[hi] > nums[idx]) {
+                    count += hi - lo
+                    --hi
+                } else
+                    ++lo
+            }
+        }
+
+        return count
+    }
+}
