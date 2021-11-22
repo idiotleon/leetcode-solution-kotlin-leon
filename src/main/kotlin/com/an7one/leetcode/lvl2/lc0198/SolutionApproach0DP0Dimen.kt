8710e990de0021f77726b46b28cb78437af0985b
Leon <yanglyu.leon.7@gmail.com>
2021-11-21 19:55:23 -0800

    review

--- src/main/kotlin/com/an7one/leetcode/lvl2/lc0198/SolutionApproach0DP0Dimen.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl2/lc0198/SolutionApproach0DP0Dimen.kt
@@ -16,8 +16,8 @@ class SolutionApproach0DP0Dimen {
     fun rob(nums: IntArray): Int {
         // sanity check
         if (nums.isEmpty()) return 0
-        val size = nums.size
-        if (size == 1) return nums[0]
+        val nNums = nums.nNums
+        if (nNums == 1) return nums[0]
 
         var inclusive = 0
         var exclusive = 0
