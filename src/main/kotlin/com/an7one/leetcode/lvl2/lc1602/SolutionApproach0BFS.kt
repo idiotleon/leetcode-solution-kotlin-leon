48ab6853680572f3cfe156ecc77be6b7ca70845f
Leon <yanglyu.leon.7@gmail.com>
2022-01-23 10:29:02 -0800

    review

--- src/main/kotlin/com/an7one/leetcode/lvl2/lc1602/SolutionApproach0BFS.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl2/lc1602/SolutionApproach0BFS.kt
@@ -9,30 +9,30 @@ package com.an7one.leetcode.lvl2.lc1602
 
 import com.an7one.leetcode.util.Constant.Annotation.UNUSED
 import com.an7one.leetcode.util.dataStructure.tree.TreeNode
-import java.util.*
 
 @Suppress(UNUSED)
 class SolutionApproach0BFS {
+    @OptIn(ExperimentalStdlibApi::class)
     fun findNearestRightNode(root: TreeNode?, u: TreeNode?): TreeNode? {
         if (u == null || root == null || u == root) return root
 
-        val queue = LinkedList<TreeNode>().also {
-            it.offer(root)
+        val queue = ArrayDeque<TreeNode>().also {
+            it.addLast(root)
         }
 
         while (queue.isNotEmpty()) {
             val size = queue.size
 
             for (sz in 0 until size) {
-                val cur = queue.poll()
+                val cur = queue.removeFirst()
 
                 if (cur == u) {
-                    return if (sz < size - 1) queue.peek()
+                    return if (sz < size - 1) queue.first()
                     else null
                 }
 
-                cur.left?.let { queue.offer(it) }
-                cur.right?.let { queue.offer(it) }
+                cur.left?.let { queue.addLast(it) }
+                cur.right?.let { queue.addLast(it) }
             }
         }
 
