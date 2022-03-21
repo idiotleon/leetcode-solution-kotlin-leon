62eebd5453808ca73c996381aeebe52dd715356c
Leon <yanglyu.leon.7@gmail.com>
2022-03-21 11:14:11 -0700

    refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    // id("org.jetbrains.kotlin.jvm") version "1.3.72"
-    kotlin("jvm") version "1.6.20-RC"
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    // kotlin("jvm") version "1.6.10"
 
     // Apply the application plugin to add support for building a CLI application.
     application
@@ -33,6 +33,9 @@ dependencies {
 
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // This dependency is used by the application.
+    implementation("com.google.guava:guava:31.1-jre")
 
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
