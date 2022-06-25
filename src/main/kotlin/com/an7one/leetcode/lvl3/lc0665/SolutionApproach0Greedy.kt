3233a239b2fb757bea3a958acc014acff8a4e76a
Leon <yanglyu.leon.7@gmail.com>
2022-06-25 11:19:30 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0665/SolutionApproach0Greedy.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0665/SolutionApproach0Greedy.kt
@@ -1,25 +1,27 @@
-/**
- * https://leetcode.com/problems/non-decreasing-array/
- *
- * Time Complexity:     O(`totalNums`)
- * Space Complexity:    O(1)
- *
- * References:
- *  https://leetcode.com/problems/non-decreasing-array/discuss/106826/JavaC++-Simple-greedy-like-solution-with-explanation/109180
- *  https://leetcode.com/problems/non-decreasing-array/discuss/106826/JavaC%2B%2B-Simple-greedy-like-solution-with-explanation
- */
 package com.an7one.leetcode.lvl3.lc0665
 
 import com.an7one.leetcode.util.Constant.Annotation.UNUSED
 
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/non-decreasing-array/
+ *
+ * Time Complexity:     O(`nNums`)
+ * Space Complexity:    O(1)
+ *
+ * References:
+ * https://leetcode.com/problems/non-decreasing-array/discuss/106826/JavaC++-Simple-greedy-like-solution-with-explanation/241578
+ * https://leetcode.com/problems/non-decreasing-array/discuss/106826/JavaC++-Simple-greedy-like-solution-with-explanation/109180
+ * https://leetcode.com/problems/non-decreasing-array/discuss/106826/JavaC%2B%2B-Simple-greedy-like-solution-with-explanation
+ */
 @Suppress(UNUSED)
 class SolutionApproach0Greedy {
     fun checkPossibility(nums: IntArray): Boolean {
-        val totalNums = nums.size
+        val nNums = nums.size
 
         var modified = 0
 
-        for (i in 1 until totalNums) {
+        for (i in 1 until nNums) {
             if (nums[i - 1] > nums[i]) {
                 if (++modified > 1) return false
                 if (i == 1 || nums[i - 2] <= nums[i]) nums[i - 1] = nums[i]
--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0665/SolutionApproach0Greedy1.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0665/SolutionApproach0Greedy1.kt
@@ -1,25 +1,26 @@
-/**
- * https://leetcode.com/problems/non-decreasing-array/
- *
- * Time Complexity:     O(`totalNums`)
- * Space Complexity:    O(1)
- *
- * References:
- *  https://leetcode.com/problems/non-decreasing-array/discuss/106849/C%2B%2B-Java-Clean-Code-6-liner-Without-Modifying-Input
- */
 package com.an7one.leetcode.lvl3.lc0665
 
 import com.an7one.leetcode.util.Constant.Annotation.UNUSED
 
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/non-decreasing-array/
+ *
+ * Time Complexity:     O(`nNums`)
+ * Space Complexity:    O(1)
+ *
+ * References:
+ * https://leetcode.com/problems/non-decreasing-array/discuss/106849/C%2B%2B-Java-Clean-Code-6-liner-Without-Modifying-Input
+ */
 @Suppress(UNUSED)
 class SolutionApproach0Greedy1 {
     fun checkPossibility(nums: IntArray): Boolean {
-        val totalNums = nums.size
+        val nNums = nums.size
 
         var modified = 0
         var prev = nums[0]
 
-        for (i in 1 until totalNums) {
+        for (i in 1 until nNums) {
             if (prev > nums[i]) {
                 if (++modified > 1) return false
 
