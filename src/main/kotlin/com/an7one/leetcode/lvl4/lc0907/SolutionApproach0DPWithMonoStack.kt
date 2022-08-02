510726a8a6b958a34489901f07b5b7d51c35ed66
Leon <yanglyu.leon.7@gmail.com>
2022-08-01 21:30:00 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl4/lc0907/SolutionApproach0DPWithMonoStack.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl4/lc0907/SolutionApproach0DPWithMonoStack.kt
@@ -1,19 +1,19 @@
+package com.an7one.leetcode.lvl4.lc0907
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/sum-of-subarray-minimums/
  *
  * Time Complexity:     O(`nNums`)
  * Space Complexity:    O(`nNums`)
  *
- * dp[idx + 1]: sum of minimum of subarrays which ends with nums[idx]
+ * `dp[idx + 1]`: sum of minimum of subarrays which ends with `nums[idx]`
  *
- * References:
- *  https://leetcode.com/problems/sum-of-subarray-minimums/discuss/170769/Java-O(n)-monotone-stack-with-DP
+ * Reference:
+ * https://leetcode.com/problems/sum-of-subarray-minimums/discuss/170769/Java-O(n)-monotone-stack-with-DP
  */
-package com.an7one.leetcode.lvl4.lc0907
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-import java.util.*
-
 @Suppress(UNUSED)
 class SolutionApproach0DPWithMonoStack {
     private companion object {
@@ -26,17 +26,17 @@ class SolutionApproach0DPWithMonoStack {
         var sum = 0
         val dp = IntArray(nNums + 1) { 0 }
 
-        val stack = LinkedList<Int>().also {
-            it.push(-1)
+        val stack = ArrayDeque<Int>().also {
+            it.addLast(-1)
         }
 
         for (idx in nums.indices) {
-            while (stack.peek() != -1 && nums[idx] <= nums[stack.peek()]) {
-                stack.pop()
+            while (stack.last() != -1 && nums[idx] <= nums[stack.last()]) {
+                stack.removeLast()
             }
 
-            dp[idx + 1] = (dp[stack.peek() + 1] + (idx - stack.peek()) * nums[idx]) % MOD
-            stack.push(idx)
+            dp[idx + 1] = (dp[stack.last() + 1] + (idx - stack.last()) * nums[idx]) % MOD
+            stack.addLast(idx)
 
             sum += dp[idx + 1]
             sum %= MOD
