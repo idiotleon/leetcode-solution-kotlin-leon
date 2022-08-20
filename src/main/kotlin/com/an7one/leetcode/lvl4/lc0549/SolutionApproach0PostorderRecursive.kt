c553b3f546953fabc77985daaa1f925314b40b42
Leon <yanglyu.leon.7@gmail.com>
2022-08-19 23:25:32 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl4/lc0549/SolutionApproach0PostorderRecursive.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl4/lc0549/SolutionApproach0PostorderRecursive.kt
@@ -1,25 +1,25 @@
+package com.an7one.leetcode.lvl4.lc0549
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+import com.an7one.leetcode.util.dataStructure.tree.TreeNode
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/
  *
  * Time Complexity:     O(N)
  * Space Complexity:    O(H)
  *
  * Reference:
- *  https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/discuss/101519/Neat-Java-Solution-Single-pass-O(n)/105240
- *  https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/discuss/101519/Neat-Java-Solution-Single-pass-O(n)
+ * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/discuss/101519/Neat-Java-Solution-Single-pass-O(n)/105240
+ * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/discuss/101519/Neat-Java-Solution-Single-pass-O(n)
  */
-package com.an7one.leetcode.lvl4.lc0549
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-import com.an7one.leetcode.util.dataStructure.tree.TreeNode
-
 @Suppress(UNUSED)
 class SolutionApproach0PostorderRecursive {
     fun longestConsecutive(root: TreeNode?): Int = postorder(root).longest
 
     private fun postorder(node: TreeNode?): Res {
-        if (node == null)
-            return Res(0, 0, 0)
+        if (node == null) return Res(0, 0, 0)
 
         val (leftInc, leftDec, leftLongest) = postorder(node.left)
         val (rightInc, rightDec, rightLongest) = postorder(node.right)
