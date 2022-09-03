a5f63e19ce8f70ee835521bdc9afca38c85d5843
Leon <yanglyu.leon.7@gmail.com>
2022-09-03 00:03:32 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl4/lc0298/SolutionApproach0PostorderRecursive.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl4/lc0298/SolutionApproach0PostorderRecursive.kt
@@ -6,10 +6,11 @@ import com.an7one.leetcode.util.Constant.Annotation.UNUSED
  * @author: Leon
  * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/
  *
- * Time Complexity:     O()
- * Space Complexity:    O()
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(H)
  *
  * Reference:
+ * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/discuss/74467/Simple-Recursive-DFS-without-global-variable/140387
  * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/discuss/74468/Easy-Java-DFS-is-there-better-time-complexity-solution/251091
  */
 @Suppress(UNUSED)
