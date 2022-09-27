4776304122d28f34f1e1564e3003395573dd4e4a
Leon <yanglyu.leon.7@gmail.com>
2022-09-26 20:37:05 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0340/SolutionApproach0SlidingWindow.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0340/SolutionApproach0SlidingWindow.kt
@@ -1,16 +1,17 @@
+package com.an7one.leetcode.lvl3.lc0340
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
  *
  * Time Complexity:     O(`lenS`)
  * Space Complexity:    O(128) ~ O(1)
  *
  * Reference:
- *  https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/discuss/80047/15-lines-java-solution-using-slide-window
+ * https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/discuss/80047/15-lines-java-solution-using-slide-window
  */
-package com.an7one.leetcode.lvl3.lc0340
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-
 @Suppress(UNUSED)
 class SolutionApproach0SlidingWindow {
     fun lengthOfLongestSubstringKDistinct(str: String, k: Int): Int {
@@ -23,12 +24,10 @@ class SolutionApproach0SlidingWindow {
         var longest = 0
 
         while (hi < lenS) {
-            if (freqs[str[hi].toInt()]++ == 0)
-                ++distinct
+            if (freqs[str[hi].toInt()]++ == 0) ++distinct
 
             while (distinct > k) {
-                if (--freqs[str[lo++].toInt()] == 0)
-                    --distinct
+                if (--freqs[str[lo++].toInt()] == 0) --distinct
             }
 
             longest = maxOf(longest, hi - lo + 1)
