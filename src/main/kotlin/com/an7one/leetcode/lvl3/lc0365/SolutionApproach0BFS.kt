4a1801c25f561154aa8f6246e15521ebc2495489
Leon <yanglyu.leon.7@gmail.com>
2022-09-28 23:54:46 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0365/SolutionApproach0BFS.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0365/SolutionApproach0BFS.kt
@@ -1,25 +1,26 @@
-/**
- * https://leetcode.com/problems/water-and-jug-problem/
- *
- * Time Complexity:     O()
- * Space Complexity:    O()
- *
- * References:
- *  https://leetcode.com/problems/water-and-jug-problem/discuss/83716/Java-Programmatic-Solution-BFS-without-GCD
- */
 package com.an7one.leetcode.lvl3.lc0365
 
 import com.an7one.leetcode.util.Constant.Annotation.UNUSED
 import java.util.*
 import kotlin.collections.HashSet
 
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/water-and-jug-problem/
+ *
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ *
+ * Reference:
+ * https://leetcode.com/problems/water-and-jug-problem/discuss/83716/Java-Programmatic-Solution-BFS-without-GCD
+ */
 @Suppress(UNUSED)
 class SolutionApproach0BFS {
     fun canMeasureWater(x: Int, y: Int, z: Int): Boolean {
         if (z < 0 || z > x + y) return false
 
-        val queue = LinkedList<Int>().also {
-            it.offer(0)
+        val queue = ArrayDeque<Int>().also {
+            it.addLast(0)
         }
 
         val seen = HashSet<Int>().also {
@@ -30,23 +31,23 @@ class SolutionApproach0BFS {
             val size = queue.size
 
             for (sz in 0 until size) {
-                val n = queue.poll()
+                val n = queue.removeFirst()
 
                 if (n + x <= x + y && seen.add(n + x)) {
-                    queue.offer(n + x)
+                    queue.addLast(n + x)
                 }
 
                 if (n + y <= x + y && seen.add(n + y)) {
-                    queue.offer(n + y)
+                    queue.addLast(n + y)
                 }
 
 
                 if (n - x >= 0 && seen.add(n - x)) {
-                    queue.offer(n - x)
+                    queue.addLast(n - x)
                 }
 
                 if (n - y >= 0 && seen.add(n - y)) {
-                    queue.offer(n - y)
+                    queue.addLast(n - y)
                 }
 
                 if (seen.contains(z)) return true
