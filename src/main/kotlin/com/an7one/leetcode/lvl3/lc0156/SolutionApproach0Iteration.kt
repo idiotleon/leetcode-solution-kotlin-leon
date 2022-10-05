2e8c3165786232b65168bfaa1178bd1e8aca79e8
Leon <yanglyu.leon.7@gmail.com>
2022-10-04 22:14:45 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0156/SolutionApproach0Iteration.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0156/SolutionApproach0Iteration.kt
@@ -1,17 +1,18 @@
-/**
- * https://leetcode.com/problems/binary-tree-upside-down/
- *
- * Time Complexity:     O(N)
- * Space Complexity:    O(1)
- *
- * References:
- *  https://leetcode.com/problems/binary-tree-upside-down/discuss/49406/Java-recursive-(O(logn)-space)-and-iterative-solutions-(O(1)-space)-with-explanation-and-figure
- */
 package com.an7one.leetcode.lvl3.lc0156
 
 import com.an7one.leetcode.util.Constant.Annotation.UNUSED
 import com.an7one.leetcode.util.dataStructure.tree.TreeNode
 
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/binary-tree-upside-down/
+ *
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(1)
+ *
+ * Reference:
+ * https://leetcode.com/problems/binary-tree-upside-down/discuss/49406/Java-recursive-(O(logn)-space)-and-iterative-solutions-(O(1)-space)-with-explanation-and-figure
+ */
 @Suppress(UNUSED)
 class SolutionApproach0Iteration {
     fun upsideDownBinaryTree(root: TreeNode?): TreeNode? {
--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0156/SolutionApproach0Recursion.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0156/SolutionApproach0Recursion.kt
@@ -1,18 +1,19 @@
-/**
- * https://leetcode.com/problems/binary-tree-upside-down/
- *
- * Time Complexity:     O(N)
- * Space Complexity:    O(H)
- *
- * References:
- *  https://leetcode.com/problems/binary-tree-upside-down/discuss/49412/Clean-Java-solution/49658
- *  https://leetcode.com/problems/binary-tree-upside-down/discuss/49412/Clean-Java-solution
- */
 package com.an7one.leetcode.lvl3.lc0156
 
 import com.an7one.leetcode.util.Constant.Annotation.UNUSED
 import com.an7one.leetcode.util.dataStructure.tree.TreeNode
 
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/binary-tree-upside-down/
+ *
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(H)
+ *
+ * Reference:
+ * https://leetcode.com/problems/binary-tree-upside-down/discuss/49412/Clean-Java-solution/49658
+ *  https://leetcode.com/problems/binary-tree-upside-down/discuss/49412/Clean-Java-solution
+ */
 @Suppress(UNUSED)
 class SolutionApproach0Recursion {
     fun upsideDownBinaryTree(root: TreeNode?) = dfs(root)
