3e4d822a557d91091c536a52f1fce52d8f977908
Leon <yanglyu.leon.7@gmail.com>
2022-10-22 22:51:25 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0452/SolutionApproach0Greedy.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0452/SolutionApproach0Greedy.kt
@@ -1,4 +1,9 @@
+package com.an7one.leetcode.lvl3.lc0452
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
  *
  * Time Complexity:     O(`nPoints` * lg(`nPoints`))
@@ -8,13 +13,9 @@
  *  1. to sort by end point
  *  2. to merge if cur[0] (start of cur) < last, otherwise, a new arrow is required (then to reset "last")
  *
- * References:
- *  http://zxi.mytechroad.com/blog/geometry/leetcode-452-minimum-number-of-arrows-to-burst-balloons/
+ * Reference:
+ * http://zxi.mytechroad.com/blog/geometry/leetcode-452-minimum-number-of-arrows-to-burst-balloons/
  */
-package com.an7one.leetcode.lvl3.lc0452
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-
 @Suppress(UNUSED)
 class SolutionApproach0Greedy {
     fun findMinArrowShots(points: Array<IntArray>): Int {
