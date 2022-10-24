aeb9b11f97065dbe85cadfd3e1edfff1dbec6c8f
Leon <yanglyu.leon.7@gmail.com>
2022-10-23 19:43:20 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0236/SolutionApproach0DFSRecursive.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0236/SolutionApproach0DFSRecursive.kt
@@ -1,17 +1,18 @@
+package com.an7one.leetcode.lvl3.lc0236
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+import com.an7one.leetcode.util.dataStructure.tree.TreeNode
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves
  *
  * Time Complexity:     O(N)
  * Space Complexity:    O(H)
  *
  * Reference:
- *  https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/discuss/334577/JavaC%2B%2BPython-Two-Recursive-Solution
+ * https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/discuss/334577/JavaC%2B%2BPython-Two-Recursive-Solution
  */
-package com.an7one.leetcode.lvl3.lc0236
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-import com.an7one.leetcode.util.dataStructure.tree.TreeNode
-
 @Suppress(UNUSED)
 class SolutionApproach0DFSRecursive {
     fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
@@ -19,17 +20,14 @@ class SolutionApproach0DFSRecursive {
     }
 
     private fun dfs(node: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
-        if (node == null || node == p || node == q)
-            return node
+        if (node == null || node == p || node == q) return node
 
         val left = dfs(node.left, p, q)
         val right = dfs(node.right, p, q)
 
-        if (left == null)
-            return right
+        if (left == null) return right
 
-        if (right == null)
-            return left
+        if (right == null) return left
 
         return node
     }
