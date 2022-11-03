41780cdc1e7af722fdf26d3eefa553b1c5658d33
Leon <yanglyu.leon.7@gmail.com>
2022-11-02 23:24:24 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0276/SolutionApproach0DP0Dimen.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0276/SolutionApproach0DP0Dimen.kt
@@ -1,14 +1,3 @@
-/**
- * https://leetcode.com/problems/paint-fence/
- *
- * Time Complexity:     O(`n`)
- * Space Complexity:    O(1)
- *
- * References:
- *  https://leetcode.com/problems/paint-fence/discuss/71156/O(n)-time-java-solution-O(1)-space/151116
- *  https://youtu.be/FLbqgyJ-70I?t=1430
- *  https://docs.google.com/presentation/d/1OeA0rsnOrklarLM5G2tHHJ6EE37HpRqD7KedzOqkihk/edit#slide=id.g8285dd8f3f_1_331
- */
 package com.an7one.leetcode.lvl3.lc0276
 
 import com.an7one.leetcode.util.Constant.Annotation.UNUSED
@@ -18,7 +7,12 @@ import com.an7one.leetcode.util.Constant.Annotation.UNUSED
  * https://leetcode.com/problems/paint-fence/
  *
  * Time Complexity:     O(`n`)
- * Space Complexity:    O(`1k`)
+ * Space Complexity:    O(1)
+ *
+ * Reference:
+ * https://leetcode.com/problems/paint-fence/discuss/71156/O(n)-time-java-solution-O(1)-space/151116
+ * https://youtu.be/FLbqgyJ-70I?t=1430
+ * https://docs.google.com/presentation/d/1OeA0rsnOrklarLM5G2tHHJ6EE37HpRqD7KedzOqkihk/edit#slide=id.g8285dd8f3f_1_331
  */
 @Suppress(UNUSED)
 class SolutionApproach0DP0Dimen {
