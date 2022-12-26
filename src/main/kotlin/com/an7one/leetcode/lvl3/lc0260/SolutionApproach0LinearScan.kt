76e05bea4e49909425e7a9dd739b5a52a0cf2e0f
Leon <yanglyu.leon.7@gmail.com>
2022-12-26 00:07:44 -0800

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0260/SolutionApproach0LinearScan.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0260/SolutionApproach0LinearScan.kt
@@ -1,7 +1,12 @@
+package com.an7one.leetcode.lvl3.lc0260
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/single-number-iii/
  *
- * Time Complexity:     O(`totalNums`)
+ * Time Complexity:     O(`nNums`)
  * Space Complexity:    O(1)
  *
  * `num and (num - 1)`, to turn the rightmost 1-bit to 0
@@ -9,20 +14,16 @@
  *
  * `num and (-num)`, to keep the rightmost 1-bit and sets all the other bits to 0
  *
- * References:
- *  https://docs.google.com/presentation/d/1_1mhjlmIwTHKV_rRpCo3YLI3Kc3FU8i4dbqhqghK6s8/edit#slide=id.ga4b64814d9_1_51
- *  https://docs.google.com/presentation/d/1_1mhjlmIwTHKV_rRpCo3YLI3Kc3FU8i4dbqhqghK6s8/edit#slide=id.ga4b64814d9_1_31
- *  https://docs.google.com/presentation/d/1_1mhjlmIwTHKV_rRpCo3YLI3Kc3FU8i4dbqhqghK6s8/edit#slide=id.ga4b64814d9_1_43
+ * Reference:
+ * https://docs.google.com/presentation/d/1_1mhjlmIwTHKV_rRpCo3YLI3Kc3FU8i4dbqhqghK6s8/edit#slide=id.ga4b64814d9_1_51
+ * https://docs.google.com/presentation/d/1_1mhjlmIwTHKV_rRpCo3YLI3Kc3FU8i4dbqhqghK6s8/edit#slide=id.ga4b64814d9_1_31
+ * https://docs.google.com/presentation/d/1_1mhjlmIwTHKV_rRpCo3YLI3Kc3FU8i4dbqhqghK6s8/edit#slide=id.ga4b64814d9_1_43
  */
-package com.an7one.leetcode.lvl3.lc0260
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-
 @Suppress(UNUSED)
 class SolutionApproach0LinearScan {
     fun singleNumber(nums: IntArray): IntArray {
         // not used
-        // val totalNums = nums.size
+        // val nNums = nums.size
 
         var diffBits = 0 // the 2 different bits left
         for (num in nums) {
@@ -39,8 +40,7 @@ class SolutionApproach0LinearScan {
         var single2 = 0
 
         for (num in nums) {
-            if ((lastDigit and num) == 0)
-                single1 = single1 xor num
+            if ((lastDigit and num) == 0) single1 = single1 xor num
             else single2 = single2 xor num
         }
 
--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0260/SolutionApproach1HashMap.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0260/SolutionApproach1HashMap.kt
@@ -1,18 +1,19 @@
-/**
- * https://leetcode.com/problems/single-number-iii/
- *
- * Time Complexity:     O(`totalNums`)
- * Space Complexity:    O(`totalNums`)
- */
 package com.an7one.leetcode.lvl3.lc0260
 
 import com.an7one.leetcode.util.Constant.Annotation.UNUSED
 
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/single-number-iii/
+ *
+ * Time Complexity:     O(`nNums`)
+ * Space Complexity:    O(`nNums`)
+ */
 @Suppress(UNUSED)
 class SolutionApproach1HashMap {
     fun singleNumber(nums: IntArray): IntArray {
         // not used
-        // val totalNums = nums.size
+        // val nNums = nums.size
 
         val freqs = HashMap<Int, Int>()
         for (num in nums) {
@@ -21,9 +22,7 @@ class SolutionApproach1HashMap {
 
         val ans = ArrayList<Int>()
         for ((num, freq) in freqs) {
-            if (freq == 1) {
-                ans.add(num)
-            }
+            if (freq == 1) ans.add(num)
         }
 
         return ans.toIntArray()
