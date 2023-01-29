7149927048620a6ed512f597a4ec5f3111fa7d40
Leon <yanglyu.leon.7@gmail.com>
2023-01-28 23:54:59 -0800

    review

--- src/main/kotlin/com/an7one/leetcode/lvl5/lc0857/SolutionApproach0Greedy.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl5/lc0857/SolutionApproach0Greedy.kt
@@ -1,4 +1,10 @@
+package com.an7one.leetcode.lvl5.lc0857
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+import java.util.*
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/minimum-cost-to-hire-k-workers/
  *
  * Time Complexity:     O(`sizeW` * lg(`sizeW`)) + O(`sizeW` * lg(k`))
@@ -7,19 +13,13 @@
  * Reference:
  * https://leetcode.com/problems/minimum-cost-to-hire-k-workers/discuss/141768/Detailed-explanation-O(NlogN)
  */
-package com.an7one.leetcode.lvl5.lc0857
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-import java.util.*
-
 @Suppress(UNUSED)
 class SolutionApproach0Greedy {
-    fun mincostToHireWorkers(qualities: IntArray, wages: IntArray, k: Int): Double {
+    fun minCostToHireWorkers(qualities: IntArray, wages: IntArray, k: Int): Double {
         val sizeW = qualities.size
         val workers = Array(sizeW) { idx ->
             Worker(
-                wages[idx].toDouble() / qualities[idx].toDouble(),
-                qualities[idx].toDouble()
+                wages[idx].toDouble() / qualities[idx].toDouble(), qualities[idx].toDouble()
             )
         }.also {
             it.sortBy { w -> w.ratio }
@@ -33,11 +33,9 @@ class SolutionApproach0Greedy {
             sumQua += quality
             maxHeap.add(quality)
 
-            if (maxHeap.size > k)
-                sumQua -= maxHeap.poll()
+            if (maxHeap.size > k) sumQua -= maxHeap.poll()
 
-            if (maxHeap.size == k)
-                minCost = minOf(minCost, sumQua * ratio)
+            if (maxHeap.size == k) minCost = minOf(minCost, sumQua * ratio)
         }
 
         return minCost
