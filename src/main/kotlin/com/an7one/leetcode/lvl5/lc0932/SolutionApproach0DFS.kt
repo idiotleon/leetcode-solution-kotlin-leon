bee58d2a040ac1866618b6853a3573073a8354a4
Leon <yanglyu.leon.7@gmail.com>
2023-01-31 23:10:55 -0800

    review

--- src/main/kotlin/com/an7one/leetcode/lvl5/lc0932/SolutionApproach0DFS.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl5/lc0932/SolutionApproach0DFS.kt
@@ -1,16 +1,16 @@
+package com.an7one.leetcode.lvl5.lc0932
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/beautiful-array/
  *
  * Time Complexity:     O(`N` * lg(`N`))
  * Space Complexity:    O(`N`)
  *
- * References:
- *  https://leetcode.com/problems/beautiful-array/discuss/187669/Share-my-O(NlogN)-C%2B%2B-solution-with-proof-and-explanation
+ * Reference:
+ * https://leetcode.com/problems/beautiful-array/discuss/187669/Share-my-O(NlogN)-C%2B%2B-solution-with-proof-and-explanation
  */
-package com.an7one.leetcode.lvl5.lc0932
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-
 @Suppress(UNUSED)
-class SolutionApproach0DFS {
-}
+class SolutionApproach0DFS {}
--- src/main/kotlin/com/an7one/leetcode/lvl5/lc0932/SolutionApproach0DivideAndConquer.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl5/lc0932/SolutionApproach0DivideAndConquer.kt
@@ -1,17 +1,18 @@
+package com.an7one.leetcode.lvl5.lc0932
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/beautiful-array/
  *
  * Time Complexity:     O(`N`)
  * Space Complexity:    O(`N`)
  *
- * References:
- *  https://leetcode.com/problems/beautiful-array/discuss/186679/Odd-%2B-Even-Pattern-O(N)
- *  https://leetcode.com/problems/beautiful-array/discuss/187669/Share-my-O(NlogN)-C%2B%2B-solution-with-proof-and-explanation
+ * Reference:
+ * https://leetcode.com/problems/beautiful-array/discuss/186679/Odd-%2B-Even-Pattern-O(N)
+ * https://leetcode.com/problems/beautiful-array/discuss/187669/Share-my-O(NlogN)-C%2B%2B-solution-with-proof-and-explanation
  */
-package com.an7one.leetcode.lvl5.lc0932
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-
 @Suppress(UNUSED)
 class SolutionApproach0DivideAndConquer {
     fun beautifulArray(n: Int): IntArray {
@@ -20,13 +21,11 @@ class SolutionApproach0DivideAndConquer {
         while (ans.size < n) {
             val temp = mutableListOf<Int>()
             for (num in ans) {
-                if (num * 2 - 1 <= n)
-                    temp.add(num * 2 - 1)
+                if (num * 2 - 1 <= n) temp.add(num * 2 - 1)
             }
 
             for (num in ans) {
-                if (num * 2 <= n)
-                    temp.add(num * 2)
+                if (num * 2 <= n) temp.add(num * 2)
             }
 
             ans = temp
