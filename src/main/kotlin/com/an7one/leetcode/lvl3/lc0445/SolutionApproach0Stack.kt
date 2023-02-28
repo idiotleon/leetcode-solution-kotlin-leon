d9487ec2e5b0226c09cbcc482fceb13f6297564f
Leon <yanglyu.leon.7@gmail.com>
2023-02-27 23:44:06 -0800

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0445/SolutionApproach0Stack.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0445/SolutionApproach0Stack.kt
@@ -4,20 +4,27 @@ import com.an7one.leetcode.util.Constant.Annotation.UNUSED
 import com.an7one.leetcode.util.dataStructure.linkedList.ListNode
 import java.util.*
 
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/add-two-numbers-ii/
+ *
+ * Time Complexity:     O(L)
+ * Space Complexity:    O(L)
+ */
 @Suppress(UNUSED)
 class SolutionApproach0Stack {
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-        val stack1 = LinkedList<Int>()
+        val stack1 = ArrayDeque<Int>()
         var cur = l1
         while (cur != null) {
-            stack1.push(cur.`val`)
+            stack1.addLast(cur.`val`)
             cur = cur.next
         }
 
-        val stack2 = LinkedList<Int>()
+        val stack2 = ArrayDeque<Int>()
         cur = l2
         while (cur != null) {
-            stack2.push(cur.`val`)
+            stack2.addLast(cur.`val`)
             cur = cur.next
         }
 
@@ -25,8 +32,8 @@ class SolutionApproach0Stack {
         var head: ListNode? = null
 
         while (stack1.isNotEmpty() || stack2.isNotEmpty() || carry > 0) {
-            val num1 = if (stack1.isNotEmpty()) stack1.pop() else 0
-            val num2 = if (stack2.isNotEmpty()) stack2.pop() else 0
+            val num1 = if (stack1.isNotEmpty()) stack1.removeLast() else 0
+            val num2 = if (stack2.isNotEmpty()) stack2.removeLast() else 0
 
             val sum = num1 + num2 + carry
             carry = sum / 10
