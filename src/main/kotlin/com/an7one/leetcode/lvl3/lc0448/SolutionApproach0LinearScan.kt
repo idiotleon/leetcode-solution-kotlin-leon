99984e8efab0f2996da2fec22ca3546e7d28ca23
Leon <yanglyu.leon.7@gmail.com>
2023-03-01 00:01:45 -0800

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0448/SolutionApproach0LinearScan.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0448/SolutionApproach0LinearScan.kt
@@ -1,16 +1,17 @@
+package com.an7one.leetcode.lvl3.lc0448
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
  *
  * Time Complexity:     O(`nNUms`)
  * Space Complexity:    O(`nNums`)
  *
- * References:
- *  https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/discuss/92980/5-line-Java-Easy-understanding
+ * Reference:
+ * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/discuss/92980/5-line-Java-Easy-understanding
  */
-package com.an7one.leetcode.lvl3.lc0448
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-
 @Suppress(UNUSED)
 class SolutionApproach0LinearScan {
     fun findDisappearedNumbers(nums: IntArray): List<Int> {
@@ -22,9 +23,7 @@ class SolutionApproach0LinearScan {
         }
 
         for (idx in nums.indices) {
-            if (nums[idx] <= nNums) {
-                ans.add(idx + 1)
-            }
+            if (nums[idx] <= nNums) ans.add(idx + 1)
         }
 
         return ans
--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0448/SolutionApproach0LinearScan1.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0448/SolutionApproach0LinearScan1.kt
@@ -1,16 +1,17 @@
+package com.an7one.leetcode.lvl3.lc0448
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
  *
  * Time Complexity:     O(`nNums`)
  * Space Complexity:    O(1)
  *
- * References:
- *  https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/discuss/93007/Simple-Java-In-place-sort-solution
+ * Reference:
+ * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/discuss/93007/Simple-Java-In-place-sort-solution
  */
-package com.an7one.leetcode.lvl3.lc0448
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-
 @Suppress(UNUSED)
 class SolutionApproach0LinearScan1 {
     fun findDisappearedNumbers(nums: IntArray): List<Int> {
