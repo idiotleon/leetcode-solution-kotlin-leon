36c2ab5312ee86c91c1dc89c1ec120b6605c8c92
Leon <yanglyu.leon.7@gmail.com>
2023-03-08 20:12:40 -0800

    review

--- src/main/kotlin/com/an7one/leetcode/lvl3/lc0450/SolutionApproach0DFSRecursive1.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl3/lc0450/SolutionApproach0DFSRecursive1.kt
@@ -1,18 +1,19 @@
-/**
- * https://leetcode.com/problems/delete-node-in-a-bst/
- *
- * Time Complexity:     O(H)
- * Space Complexity:    O(H)
- *
- * References:
- *  https://leetcode.com/problems/delete-node-in-a-bst/discuss/93296/Recursive-Easy-to-Understand-Java-Solution/97795
- *  https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247488128&idx=2&sn=b8fb3fd2917f9ac86127054741cd5877&chksm=9bd7ec88aca0659ee0185b657663169169493e9df2063fa4d28b38a0b4d0dd698d0301937898&scene=21#wechat_redirect
- */
 package com.an7one.leetcode.lvl3.lc0450
 
 import com.an7one.leetcode.util.Constant.Annotation.UNUSED
 import com.an7one.leetcode.util.dataStructure.tree.TreeNode
 
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/delete-node-in-a-bst/
+ *
+ * Time Complexity:     O(H)
+ * Space Complexity:    O(H)
+ *
+ * Reference:
+ * https://leetcode.com/problems/delete-node-in-a-bst/discuss/93296/Recursive-Easy-to-Understand-Java-Solution/97795
+ * https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247488128&idx=2&sn=b8fb3fd2917f9ac86127054741cd5877&chksm=9bd7ec88aca0659ee0185b657663169169493e9df2063fa4d28b38a0b4d0dd698d0301937898&scene=21#wechat_redirect
+ */
 @Suppress(UNUSED)
 class SolutionApproach0DFSRecursive1 {
     fun deleteNode(root: TreeNode?, key: Int) = delete(root, key)
