551cc3769bd4f07fb699097a04136313fbc0f2b3
Leon <yanglyu.leon.7@gmail.com>
2023-03-14 19:13:48 -0700

    review

--- src/main/kotlin/com/an7one/leetcode/lvl2/lc0958/SolutionApproach0BFS.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl2/lc0958/SolutionApproach0BFS.kt
@@ -1,18 +1,19 @@
+package com.an7one.leetcode.lvl2.lc0958
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+import com.an7one.leetcode.util.dataStructure.tree.TreeNode
+import java.util.LinkedList
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/check-completeness-of-a-binary-tree/
  *
  * Time Complexity:     O(N)
  * Space Complexity:    O(W)
  *
  * Reference:
- *  https://leetcode.com/problems/check-completeness-of-a-binary-tree/discuss/205768/Java-easy-Level-Order-Traversal-one-while-loop
+ * https://leetcode.com/problems/check-completeness-of-a-binary-tree/discuss/205768/Java-easy-Level-Order-Traversal-one-while-loop
  */
-package com.an7one.leetcode.lvl2.lc0958
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-import com.an7one.leetcode.util.dataStructure.tree.TreeNode
-import java.util.*
-
 @Suppress(UNUSED)
 class SolutionApproach0BFS {
     fun isCompleteTree(root: TreeNode?): Boolean {
@@ -25,11 +26,9 @@ class SolutionApproach0BFS {
         while (queue.isNotEmpty()) {
             val cur = queue.poll()
 
-            if (cur == null)
-                isEnd = true
+            if (cur == null) isEnd = true
             else {
-                if (isEnd)
-                    return false
+                if (isEnd) return false
 
                 queue.offer(cur!!.left)
                 queue.offer(cur!!.right)
--- src/main/kotlin/com/an7one/leetcode/lvl2/lc0958/SolutionApproach0DFSRecursive.kt
+++ src/main/kotlin/com/an7one/leetcode/lvl2/lc0958/SolutionApproach0DFSRecursive.kt
@@ -1,16 +1,16 @@
+package com.an7one.leetcode.lvl2.lc0958
+
+import com.an7one.leetcode.util.Constant.Annotation.UNUSED
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/check-completeness-of-a-binary-tree/
  *
  * Time Complexity:     O(N)
  * Space Complexity:    O(H)
  *
  * Reference:
- *  https://leetcode.com/problems/check-completeness-of-a-binary-tree/discuss/205682/JavaC++Python-BFS-Level-Order-Traversal/209403
+ * https://leetcode.com/problems/check-completeness-of-a-binary-tree/discuss/205682/JavaC++Python-BFS-Level-Order-Traversal/209403
  */
-package com.an7one.leetcode.lvl2.lc0958
-
-import com.an7one.leetcode.util.Constant.Annotation.UNUSED
-
 @Suppress(UNUSED)
-class SolutionApproach0DFSRecursive {
-}
+class SolutionApproach0DFSRecursive {}
