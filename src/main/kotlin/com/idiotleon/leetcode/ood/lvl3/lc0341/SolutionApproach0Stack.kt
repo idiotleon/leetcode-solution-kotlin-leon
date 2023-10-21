6eff8a619d0791c31d25f931678dcc0e32e31451
Leon <yanglyu.leon.7@gmail.com>
2023-10-21 14:10:17 -0700

    review

--- src/main/kotlin/com/idiotleon/leetcode/ood/lvl3/lc0341/SolutionApproach0Stack.kt
+++ src/main/kotlin/com/idiotleon/leetcode/ood/lvl3/lc0341/SolutionApproach0Stack.kt
@@ -1,23 +1,25 @@
+package com.idiotleon.leetcode.ood.lvl3.lc0341
+
+import com.idiotleon.leetcode.util.Constant.Annotation.UNUSED
+import java.util.*
+import kotlin.collections.ArrayDeque
+
 /**
+ * @author: Leon
  * https://leetcode.com/problems/flatten-nested-list-iterator/
  *
- * Time Complexities:
+ * Time Complexity:
  *  `next()`:       O()
  *  `hasNext()`:    O()
  *
  * Space Complexity:    O()
  *
- * References:
- *  https://leetcode.com/problems/flatten-nested-list-iterator/discuss/80147/Simple-Java-solution-using-a-stack-with-explanation
+ * Reference:
+ * https://leetcode.com/problems/flatten-nested-list-iterator/discuss/80147/Simple-Java-solution-using-a-stack-with-explanation
  */
-package com.idiotleon.leetcode.ood.lvl3.lc0341
-
-import com.idiotleon.leetcode.util.Constant.Annotation.UNUSED
-import java.util.*
-
 @Suppress(UNUSED)
 class SolutionApproach0Stack(nestedList: List<NestedInteger>) {
-    private val stack = LinkedList<NestedInteger>()
+    private val stack = ArrayDeque<NestedInteger>()
 
     init {
         flattenNestedList(nestedList)
@@ -26,12 +28,12 @@ class SolutionApproach0Stack(nestedList: List<NestedInteger>) {
     fun next(): Int {
         if (!hasNext()) return -1
 
-        return stack.pop().getInteger()!!
+        return stack.removeLast().getInteger()!!
     }
 
     fun hasNext(): Boolean {
-        while (stack.isNotEmpty() && !stack.peek().isInteger()) {
-            val list = stack.pop().getList()
+        while (stack.isNotEmpty() && !stack.last().isInteger()) {
+            val list = stack.removeLast().getList()
             flattenNestedList(list)
         }
 
@@ -41,7 +43,7 @@ class SolutionApproach0Stack(nestedList: List<NestedInteger>) {
     private fun flattenNestedList(nestedList: List<NestedInteger>?) {
         nestedList?.let {
             for (idx in it.indices.reversed()) {
-                stack.push(it[idx])
+                stack.addLast(it[idx])
             }
         }
     }
