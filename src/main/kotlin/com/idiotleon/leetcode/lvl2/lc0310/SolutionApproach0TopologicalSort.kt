24d7155b00b04a748c3f609725e4d8457db88440
Leon <yanglyu.leon.7@gmail.com>
2023-11-01 10:50:54 -0700

    review

--- src/main/kotlin/com/idiotleon/leetcode/lvl2/lc0310/SolutionApproach0TopologicalSort.kt
+++ src/main/kotlin/com/idiotleon/leetcode/lvl2/lc0310/SolutionApproach0TopologicalSort.kt
@@ -1,4 +1,4 @@
-package com.idiotleon.leetcode.lvl3.lc0310
+package com.idiotleon.leetcode.lvl2.lc0310
 
 import com.idiotleon.leetcode.util.Constant.Annotation.UNUSED
 
@@ -8,8 +8,8 @@ import kotlin.collections.ArrayDeque;
  * @author: Leon
  * https://leetcode.com/problems/minimum-height-trees/
  *
- * Time Complexity:     O(`n`)
- * Space Complexity:    O(`n`)
+ * Time Complexity:     O(V + E) ~ O(`n` + `nEdges`) ~ O(`n`)
+ * Space Complexity:    O(V + E) ~ O(`n` + `nEdges`) ~ O(`n`)
  *
  * Reference:
  * https://leetcode.com/problems/minimum-height-trees/discuss/76129/Share-my-BFS-JAVA-code-using-degree-with-explanation-which-beats-more-than-95/293110
@@ -17,7 +17,12 @@ import kotlin.collections.ArrayDeque;
 @Suppress(UNUSED)
 class SolutionApproach0TopologicalSort {
     fun findMinHeightTrees(n: Int, edges: Array<IntArray>): List<Int> {
-        if (n == 1) return mutableListOf(0)
+        // not used
+        // val nEdges = edges.size
+
+        if (n == 1) {
+            return mutableListOf(0)
+        }
 
         val degrees = IntArray(n) { 0 }
         val graph = buildGraph(degrees, n, edges)
@@ -34,7 +39,7 @@ class SolutionApproach0TopologicalSort {
             val size = queue.size
             val newLeaves = mutableListOf<Int>()
 
-            for (_sz in 0 until size) {
+            for (sz in 0 until size) {
                 val cur = queue.removeFirst()
                 newLeaves.add(cur)
 
