6b680768cbe7d06b401d45e84d27cbc0107681be
Leon <yanglyu.leon.7@gmail.com>
2025-01-05 23:32:32 -0800

    refactor

---
+++ src/main/kotlin/com/idiotleon/leetcode/lvl4/lc0314/Solution0Bfs.kt
@@ -0,0 +1,13 @@
+package com.idiotleon.leetcode.lvl4.lc0314
+
+import com.idiotleon.leetcode.util.Constant.Annotation.UNUSED
+
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/binary-tree-vertical-order-traversal/
+ *
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(W)
+ */
+@Suppress(UNUSED)
+class Solution0Bfs {}
