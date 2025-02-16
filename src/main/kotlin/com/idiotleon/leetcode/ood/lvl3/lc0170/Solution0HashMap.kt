e1544f16832f1a4d8a48cc25b58a8c7df571cdca
Leon <yanglyu.leon.7@gmail.com>
2025-02-15 23:22:27 -0800

    refactor

--- src/main/kotlin/com/idiotleon/leetcode/ood/lvl3/lc0170/Solution0HashMap.kt
+++ src/main/kotlin/com/idiotleon/leetcode/ood/lvl3/lc0170/Solution0HashMap.kt
@@ -1,24 +1,24 @@
+package com.idiotleon.leetcode.ood.lvl3.lc0170
+
+import com.idiotleon.leetcode.util.Constant.Annotation.UNUSED
+
 /**
  * https://leetcode.com/problems/two-sum-iii-data-structure-design/
  *
  * Time Complexity:     O()
  * Space Complexity:    O()
  *
- * References:
- *  https://leetcode.com/problems/two-sum-iii-data-structure-design/discuss/52005/Trade-off-in-this-problem-should-be-considered/115286
+ * Reference:
+ * https://leetcode.com/problems/two-sum-iii-data-structure-design/discuss/52005/Trade-off-in-this-problem-should-be-considered/115286
  */
-package com.idiotleon.leetcode.ood.lvl3.lc0170
-
-import com.idiotleon.leetcode.util.Constant.Annotation.UNUSED
-
 @Suppress(UNUSED)
-class SolutionApproach0HashMap {
+class Solution0HashMap {
     private val freqs = hashMapOf<Int, Int>()
 
     /** Initialize your data structure here. */
 
 
-    /** Add the number to an internal data structure.. */
+    /** Add the number to an internal data structure. */
     fun add(num: Int) {
         freqs[num] = (freqs[num] ?: 0) + 1
     }
@@ -27,9 +27,9 @@ class SolutionApproach0HashMap {
     fun find(value: Int): Boolean {
         for ((num1, freq) in freqs) {
             val num2 = value - num1
-            if ((num1 == num2) && (freq > 1)
-                || (num1 != num2) && (freqs[num2] != null)
-            ) return true
+            if ((num1 == num2) && (freq > 1) || (num1 != num2) && (freqs[num2] != null)) {
+                return true
+            }
         }
 
         return false
