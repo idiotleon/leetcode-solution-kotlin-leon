5e60796f0ad6302ab04f1866f97f96e9a9ca1270
Leon <yanglyu.leon.7@gmail.com>
2025-02-18 23:16:17 -0800

    refactor

--- src/main/kotlin/com/idiotleon/leetcode/lvl3/lc0055/Solution0Greedy.kt
+++ src/main/kotlin/com/idiotleon/leetcode/lvl3/lc0055/Solution0Greedy.kt
@@ -10,7 +10,7 @@ import com.idiotleon.leetcode.util.Constant.Annotation.UNUSED
  * Space Complexity:    O(1)
  */
 @Suppress(UNUSED)
-class SolutionApproach0Greedy {
+class Solution0Greedy {
     fun canJump(nums: IntArray): Boolean {
         val nNums = nums.size
         var furthest = 0
